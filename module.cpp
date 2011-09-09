4cf831a651f3975c62a04bd40edeb40d2fc7485f
Matt Pharr <matt.pharr@intel.com>
2011-09-09 09:32:59 -0700

    When --fast-math is enabled, tell LLVM about it, too.

--- module.cpp
+++ module.cpp
@@ -153,6 +153,9 @@ extern void yy_delete_buffer(YY_BUFFER_STATE);
 
 int
 Module::CompileFile() {
+    if (g->opt.fastMath == true)
+        llvm::UnsafeFPMath = true;
+
     // FIXME: it'd be nice to do this in the Module constructor, but this
     // function ends up calling into routines that expect the global
     // variable 'm' to be initialized and available (which it isn't until
