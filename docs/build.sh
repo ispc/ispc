4d6bcdf41c8007751391215d16f09e9387d52644
Matt Pharr <matt.pharr@intel.com>
2011-11-30 17:11:03 -0800

    Documentation refactoring, initial pass at FAQ

--- docs/build.sh
+++ docs/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rst2html.py ispc.txt > ispc.html
+rst2html.py perf.txt > perf.html
+rst2html.py faq.txt > faq.html
 
 #rst2latex --section-numbering --documentclass=article --documentoptions=DIV=9,10pt,letterpaper ispc.txt > ispc.tex
 #pdflatex ispc.tex
