fda00afe6eba57dbeb0902ad2c0c146533627091
Matt Pharr <matt.pharr@intel.com>
2012-01-08 14:11:04 -0800

    Rename .txt files in docs to .rst (which is what they actually are).

--- docs/build.sh
+++ docs/build.sh
@@ -2,11 +2,11 @@
 
 for i in ispc perfguide faq; do
     rst2html.py --template=template.txt --link-stylesheet \
-        --stylesheet-path=css/style.css $i.txt > $i.html
+        --stylesheet-path=css/style.css $i.rst > $i.html
 done
 
 rst2html.py --template=template-perf.txt --link-stylesheet \
-        --stylesheet-path=css/style.css perf.txt > perf.html
+        --stylesheet-path=css/style.css perf.rst > perf.html
 
 #rst2latex --section-numbering --documentclass=article --documentoptions=DIV=9,10pt,letterpaper ispc.txt > ispc.tex
 #pdflatex ispc.tex
