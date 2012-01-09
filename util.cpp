a0e9793de31c87bc610c91c32bbc957178d1a972
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2012-01-09 10:19:46 +0100

    Shut up warning wrt CONSOLE_SCREEN_BUFFER_INFO initialization

--- util.cpp
+++ util.cpp
@@ -78,7 +78,7 @@ lTerminalWidth() {
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
     if (h == INVALID_HANDLE_VALUE || h == NULL)
         return 80;
-    CONSOLE_SCREEN_BUFFER_INFO bufferInfo = { 0 };
+    CONSOLE_SCREEN_BUFFER_INFO bufferInfo = { {0} };
     GetConsoleScreenBufferInfo(h, &bufferInfo);
     return bufferInfo.dwSize.X;
 #else
