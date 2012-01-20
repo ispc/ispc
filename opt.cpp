ispc/ispc

commit be0c77d556266af3c4ddc18ca313f99ddf68d9c7

Detect more gather/scatter cases that are actually base+offsets.
We now recognize patterns like (ptr + offset1 + offset2) as being
cases we can handle with the base_offsets variants of the gather/scatter
functions.  (This can come up with multidimensional array indexing,
for example.)

Issue #150.

opt.cpp
@@ -1063,7 +1063,8 @@ lGetConstantAddExprBaseOffset(llvm::Constant *op0, llvm::Constant *op1,
     *offsets with an int vector of the per-lane offsets
  */
 static llvm::Value *
-lGetBasePtrAndOffsets(llvm::Value *ptrs, llvm::Value **offsets) {
+lGetBasePtrAndOffsets(llvm::Value *ptrs, llvm::Value **offsets,
+                      llvm::Instruction *insertBefore) {
     llvm::Value *base = lGetBasePointer(ptrs);
     if (base != NULL) {
         // We have a straight up varying pointer with no indexing that's
@@ -1079,12 +1080,20 @@ lGetBasePtrAndOffsets(llvm::Value *ptrs, llvm::Value **offsets) {
     if (bop != NULL && bop->getOpcode() == llvm::Instruction::Add) {
         // If we have a common pointer plus something, then we're also
         // good.
-        if ((base = lGetBasePointer(bop->getOperand(0))) != NULL) {
-            *offsets = bop->getOperand(1);
+        if ((base = lGetBasePtrAndOffsets(bop->getOperand(0), 
+                                          offsets, insertBefore)) != NULL) {
+            *offsets = 
+                llvm::BinaryOperator::Create(llvm::Instruction::Add, *offsets,
+                                             bop->getOperand(1), "new_offsets",
+                                             insertBefore);
             return base;
         }
-        else if ((base = lGetBasePointer(bop->getOperand(1))) != NULL) {
-            *offsets = bop->getOperand(0);
+        else if ((base = lGetBasePtrAndOffsets(bop->getOperand(1), 
+                                               offsets, insertBefore)) != NULL) {
+            *offsets = 
+                llvm::BinaryOperator::Create(llvm::Instruction::Add, *offsets,
+                                             bop->getOperand(0), "new_offsets",
+                                             insertBefore);
             return base;
         }
     }
@@ -1316,7 +1325,8 @@ GatherScatterFlattenOpt::runOnBasicBlock(llvm::BasicBlock &bb) {
         // lGetBasePtrAndOffsets).
         llvm::Value *ptrs = callInst->getArgOperand(0);
         llvm::Value *offsetVector = NULL;
-        llvm::Value *basePtr = lGetBasePtrAndOffsets(ptrs, &offsetVector);
+        llvm::Value *basePtr = lGetBasePtrAndOffsets(ptrs, &offsetVector, 
+                                                     callInst);
 
         if (basePtr == NULL || offsetVector == NULL)
             // It's actually a fully general gather/scatter with a varying

commit 4388338dadf84114186a4e3d86a1c13602631740

Fix performance regression introduced in be0c77d5562
Effectively, the patterns that detected when given a gather or
scatter in base+offsets form, the offsets were actually a multiple
of 2/4/8, were no longer working.

This change not only fixes this, but also expands the set of
patterns that are matched by this.  For example, given offsets of
the form 4*v1 + 16*v2, it identifies a scale of 4 and new offsets
of v1 + 4*v2.

This fix makes the volume renderer run 1.19x faster, and noise 1.54x
faster.

opt.cpp
@@ -1174,21 +1174,47 @@ lExtractOffsetVector248Scale(llvm::Value **vec) {
         return scale;
     }
 
-    // If we don't have a multiply, then just return
+
+    // If we don't have a binary operator, then just give up
     llvm::BinaryOperator *bop = llvm::dyn_cast<llvm::BinaryOperator>(*vec);
-    if (bop == NULL || bop->getOpcode() != llvm::Instruction::Mul)
+    if (bop == NULL)
         return LLVMInt32(1);
 
-    // Check each operand for being one of the scale factors we care about.
     llvm::Value *op0 = bop->getOperand(0), *op1 = bop->getOperand(1);
-    int splat;
-    if (lIs248Splat(op0, &splat)) {
-        *vec = op1;
-        return LLVMInt32(splat);
+    if (bop->getOpcode() == llvm::Instruction::Add) {
+        if (llvm::isa<llvm::ConstantAggregateZero>(op0)) {
+            *vec = op1;
+            return lExtractOffsetVector248Scale(vec);
+        }
+        else if (llvm::isa<llvm::ConstantAggregateZero>(op1)) {
+            *vec = op0;
+            return lExtractOffsetVector248Scale(vec);
+        }
+        else {
+            llvm::Value *s0 = lExtractOffsetVector248Scale(&op0);
+            llvm::Value *s1 = lExtractOffsetVector248Scale(&op1);
+            if (s0 == s1) {
+                *vec = llvm::BinaryOperator::Create(llvm::Instruction::Add,
+                                                    op0, op1, "new_add", bop);
+                return s0;
+            }
+            else
+                return LLVMInt32(1);
+        }
     }
-    else if (lIs248Splat(op1, &splat)) {
-        *vec = op0;
-        return LLVMInt32(splat);
+    else if (bop->getOpcode() == llvm::Instruction::Mul) {
+        // Check each operand for being one of the scale factors we care about.
+        int splat;
+        if (lIs248Splat(op0, &splat)) {
+            *vec = op1;
+            return LLVMInt32(splat);
+        }
+        else if (lIs248Splat(op1, &splat)) {
+            *vec = op0;
+            return LLVMInt32(splat);
+        }
+        else
+            return LLVMInt32(1);
     }
     else
         return LLVMInt32(1);