65f3252760983a6dcd40778f2df7f3e98ccc1859
Matt Pharr <matt.pharr@intel.com>
2012-01-26 10:55:27 -0800

    Various fixes to test running script for Windows.
    Also, removed the --valgrind option and replaced it with a more
    general --wrap-exe option, which can be used both for running
    Valgrind and SDE.

--- winstuff/stdint.h
+++ winstuff/stdint.h
@@ -1,7 +1,9 @@
 #ifndef MY_STDINT_H
 #define MY_STDINT_H 1
 
+typedef unsigned __int32 uint32_t;
 typedef __int32 int32_t;
 typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
 
 #endif // MY_STDINT_H
