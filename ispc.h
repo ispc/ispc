6aad4c7a390866a7b1cd8dea77b59d1ae414cbf1
Matt Pharr <matt.pharr@intel.com>
2012-07-05 13:35:34 -0700

    Bump version number to 1.3.1dev

--- ispc.h
+++ ispc.h
@@ -38,7 +38,7 @@
 #ifndef ISPC_H
 #define ISPC_H
 
-#define ISPC_VERSION "1.3.0"
+#define ISPC_VERSION "1.3.1dev"
 
 #if !defined(LLVM_3_0) && !defined(LLVM_3_1) && !defined(LLVM_3_2)
 #error "Only LLVM 3.0, 3.1, and the 3.2 development branch are supported"
