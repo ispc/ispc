ispc/ispc

commit aecd6e0878d9a014d853c5307b569f75235bff8b

All the smear(), setzero() and undef() APIs are now templated on the return type. Modified ISPC's internal mangling to pass these through unchanged. Tried hard to make sure this is not going to introduce an ABI change.

cbackend.cpp
@@ -523,7 +523,7 @@ static std::string CBEMangle(const std::string &S) {
   std::string Result;
 
   for (unsigned i = 0, e = S.size(); i != e; ++i)
-    if (isalnum(S[i]) || S[i] == '_') {
+    if (isalnum(S[i]) || S[i] == '_' || S[i] == '<' || S[i] == '>') {
       Result += S[i];
     } else {
       Result += '_';
@@ -1115,29 +1115,35 @@ bool CWriter::printCast(unsigned opc, llvm::Type *SrcTy, llvm::Type *DstTy) {
 
 
 /** Construct the name of a function with the given base and returning a
-    vector of a given type.  For example, if base is "foo" and matchType is
-    i16, this will return the string "__foo_i16".
+    vector of a given type, of the specified idth.  For example, if base 
+    is "foo" and matchType is i32 and width is 16, this will return the 
+    string "__foo_i32<__vec16_i32>".
  */
 static const char *
-lGetTypedFunc(const char *base, llvm::Type *matchType) {
-    char buf[64];
-    sprintf(buf, "__%s_", base);
+lGetTypedFunc(const char *base, llvm::Type *matchType, int width) {
+    static const char *ty_desc_str[] = {"f", "d", "i1", "i8", "i16", "i32", "i64"};
+    static const char *fn_desc_str[] = {"float", "double", "i1", "i8", "i16", "i32", "i64"};
+    enum {DESC_FLOAT, DESC_DOUBLE, DESC_I1, DESC_I8, DESC_I16, DESC_I32, DESC_I64} desc;
+
     switch (matchType->getTypeID()) {
-    case llvm::Type::FloatTyID:  strcat(buf, "float");  break;
-    case llvm::Type::DoubleTyID: strcat(buf, "double"); break;
+    case llvm::Type::FloatTyID:  desc = DESC_FLOAT; break;
+    case llvm::Type::DoubleTyID: desc = DESC_DOUBLE; break;
     case llvm::Type::IntegerTyID: {
         switch (llvm::cast<llvm::IntegerType>(matchType)->getBitWidth()) {
-        case 1:  strcat(buf, "i1");  break;
-        case 8:  strcat(buf, "i8");  break;
-        case 16: strcat(buf, "i16"); break;
-        case 32: strcat(buf, "i32"); break;
-        case 64: strcat(buf, "i64"); break;
+        case 1:  desc = DESC_I1;  break;
+        case 8:  desc = DESC_I8;  break;
+        case 16: desc = DESC_I16; break;
+        case 32: desc = DESC_I32; break;
+        case 64: desc = DESC_I64; break;
         default: return NULL;
         }
         break;
     }
     default: return NULL;
     }
+
+    char buf[64];
+    snprintf(buf, 64, "__%s_%s<__vec%d_%s>", base, fn_desc_str[desc], width, ty_desc_str[desc]);
     return strdup(buf);
 }
 
@@ -1486,19 +1492,19 @@ void CWriter::printConstant(llvm::Constant *CPV, bool Static) {
 
     if (llvm::isa<llvm::ConstantAggregateZero>(CPV)) {
         // All zeros; call the __setzero_* function.
-        const char *setZeroFunc = lGetTypedFunc("setzero", VT->getElementType());
+        const char *setZeroFunc = lGetTypedFunc("setzero", VT->getElementType(), vectorWidth);
         assert(setZeroFunc != NULL);
         Out << setZeroFunc << "()";
     }
     else if (llvm::isa<llvm::UndefValue>(CPV)) {
         // Undefined value; call __undef_* so that we can potentially pass
         // this information along..
-        const char *undefFunc = lGetTypedFunc("undef", VT->getElementType());
+        const char *undefFunc = lGetTypedFunc("undef", VT->getElementType(), vectorWidth);
         assert(undefFunc != NULL);
         Out << undefFunc << "()";
     }
     else {
-        const char *smearFunc = lGetTypedFunc("smear", VT->getElementType());
+        const char *smearFunc = lGetTypedFunc("smear", VT->getElementType(), vectorWidth);
 
         if (llvm::ConstantVector *CV = llvm::dyn_cast<llvm::ConstantVector>(CPV)) {
             llvm::Constant *splatValue = CV->getSplatValue();
@@ -1713,8 +1719,8 @@ void CWriter::printConstantWithCast(llvm::Constant* CPV, unsigned Opcode) {
 std::string CWriter::GetValueName(const llvm::Value *Operand) {
 
   // Resolve potential alias.
-    if (const llvm::GlobalAlias *GA = llvm::dyn_cast<llvm::GlobalAlias>(Operand)) {
-      if (const llvm::Value *V = GA->resolveAliasedGlobal(false))
+  if (const llvm::GlobalAlias *GA = llvm::dyn_cast<llvm::GlobalAlias>(Operand)) {
+    if (const llvm::Value *V = GA->resolveAliasedGlobal(false))
       Operand = V;
   }
 
@@ -4362,12 +4368,11 @@ SmearCleanupPass::runOnBasicBlock(llvm::BasicBlock &bb) {
 
         {
         llvm::Type *matchType = toMatch->getType();
-        const char *smearFuncName = lGetTypedFunc("smear", matchType);
-
+        const char *smearFuncName = lGetTypedFunc("smear", matchType, vectorWidth);
         if (smearFuncName != NULL) {
             llvm::Function *smearFunc = module->getFunction(smearFuncName);
             if (smearFunc == NULL) {
-                // Declare the smar function if needed; it takes a single
+                // Declare the smear function if needed; it takes a single
                 // scalar parameter and returns a vector of the same
                 // parameter type.
                 llvm::Constant *sf = 

examples/intrinsics/generic-16.h
@@ -271,7 +271,8 @@ static FORCEINLINE TYPE NAME(TYPE a, int32_t b) {                   \
 }
 
 #define SMEAR(VTYPE, NAME, STYPE)                                  \
-static FORCEINLINE VTYPE __smear_##NAME(STYPE v) {                 \
+template <class RetVecType> VTYPE __smear_##NAME(STYPE);           \
+template <> FORCEINLINE VTYPE __smear_##NAME<VTYPE>(STYPE v) {     \
     VTYPE ret;                                                     \
     for (int i = 0; i < 16; ++i)                                   \
         ret.v[i] = v;                                              \
@@ -279,7 +280,8 @@ static FORCEINLINE VTYPE __smear_##NAME(STYPE v) {                 \
 }
 
 #define SETZERO(VTYPE, NAME)                                       \
-static FORCEINLINE VTYPE __setzero_##NAME() {                      \
+template <class RetVecType> VTYPE __setzero_##NAME();              \
+template <> FORCEINLINE VTYPE __setzero_##NAME<VTYPE>() {          \
     VTYPE ret;                                                     \
     for (int i = 0; i < 16; ++i)                                   \
         ret.v[i] = 0;                                              \
@@ -287,7 +289,8 @@ static FORCEINLINE VTYPE __setzero_##NAME() {                      \
 }
 
 #define UNDEF(VTYPE, NAME)                                         \
-static FORCEINLINE VTYPE __undef_##NAME() {                        \
+template <class RetVecType> VTYPE __undef_##NAME();                \
+template <> FORCEINLINE VTYPE __undef_##NAME<VTYPE>() {            \
     return VTYPE();                                                \
 }
 
@@ -416,18 +419,20 @@ template <int ALIGN> static FORCEINLINE void __store(__vec16_i1 *p, __vec16_i1 v
     *ptr = v.v;
 }
 
-static FORCEINLINE __vec16_i1 __smear_i1(int v) {
+template <class RetVecType> __vec16_i1 __smear_i1(int i);
+template <> FORCEINLINE __vec16_i1 __smear_i1<__vec16_i1>(int v) {
     return __vec16_i1(v, v, v, v, v, v, v, v, 
                       v, v, v, v, v, v, v, v);
 }
 
-static FORCEINLINE __vec16_i1 __setzero_i1() {
+template <class RetVecType> __vec16_i1 __setzero_i1();
+template <> FORCEINLINE __vec16_i1 __setzero_i1<__vec16_i1>() {
     return __vec16_i1(0, 0, 0, 0, 0, 0, 0, 0, 
                       0, 0, 0, 0, 0, 0, 0, 0);
 }
 
-
-static FORCEINLINE __vec16_i1 __undef_i1() {
+template <class RetVecType> __vec16_i1 __undef_i1();
+template <> FORCEINLINE __vec16_i1 __undef_i1<__vec16_i1>() {
     return __vec16_i1();
 }
 

examples/intrinsics/generic-32.h
@@ -336,15 +336,17 @@ static FORCEINLINE TYPE NAME(TYPE a, int32_t b) {                   \
 }
 
 #define SMEAR(VTYPE, NAME, STYPE)                                  \
-static FORCEINLINE VTYPE __smear_##NAME(STYPE v) {                 \
+template <class RetVecType> VTYPE __smear_##NAME(STYPE);           \
+template <> FORCEINLINE VTYPE __smear_##NAME<VTYPE>(STYPE v) {     \
     VTYPE ret;                                                     \
     for (int i = 0; i < 32; ++i)                                   \
         ret.v[i] = v;                                              \
     return ret;                                                    \
 }
 
-#define SETZERO(VTYPE, NAME)                   \
-static FORCEINLINE VTYPE __setzero_##NAME() {  \
+#define SETZERO(VTYPE, NAME)                                       \
+template <class RetVecType> VTYPE __setzero_##NAME();              \
+template <> FORCEINLINE VTYPE __setzero_##NAME<VTYPE>() {          \
     VTYPE ret;                                                     \
     for (int i = 0; i < 32; ++i)                                   \
         ret.v[i] = 0;                                              \
@@ -352,7 +354,8 @@ static FORCEINLINE VTYPE __setzero_##NAME() {  \
 }
 
 #define UNDEF(VTYPE, NAME)                                         \
-static FORCEINLINE VTYPE __undef_##NAME() {                        \
+template <class RetVecType> VTYPE __undef_##NAME();                \
+template <> FORCEINLINE VTYPE __undef_##NAME<VTYPE>() {                 \
     return VTYPE();                                                \
 }
 
@@ -481,21 +484,24 @@ template <int ALIGN> static FORCEINLINE void __store(__vec32_i1 *p, __vec32_i1 v
     *ptr = v.v;
 }
 
-static FORCEINLINE __vec32_i1 __smear_i1(int v) {
+template <class RetVecType> __vec32_i1 __smear_i1(int i);
+template <> FORCEINLINE __vec32_i1 __smear_i1<__vec32_i1>(int v) {
     return __vec32_i1(v, v, v, v, v, v, v, v, 
                       v, v, v, v, v, v, v, v,
                       v, v, v, v, v, v, v, v,
                       v, v, v, v, v, v, v, v);
 }
 
-static FORCEINLINE __vec32_i1 __setzero_i1() {
+template <class RetVecType> __vec32_i1 __setzero_i1();
+template <> FORCEINLINE __vec32_i1 __setzero_i1<__vec32_i1>() {
     return __vec32_i1(0, 0, 0, 0, 0, 0, 0, 0, 
                       0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0);
 }
 
-static FORCEINLINE __vec32_i1 __undef_i1() {
+template <class RetVecType> __vec32_i1 __undef_i1();
+template <> FORCEINLINE __vec32_i1 __undef_i1<__vec32_i1>() {
     return __vec32_i1();
 }
 

examples/intrinsics/generic-64.h
@@ -461,7 +461,8 @@ static FORCEINLINE TYPE NAME(TYPE a, int32_t b) {                   \
 }
 
 #define SMEAR(VTYPE, NAME, STYPE)                                  \
-static FORCEINLINE VTYPE __smear_##NAME(STYPE v) {                 \
+template <class RetVecType> VTYPE __smear_##NAME(STYPE);           \
+template <> FORCEINLINE VTYPE __smear_##NAME<VTYPE>(STYPE v) {          \
     VTYPE ret;                                                     \
     for (int i = 0; i < 64; ++i)                                   \
         ret.v[i] = v;                                              \
@@ -469,7 +470,8 @@ static FORCEINLINE VTYPE __smear_##NAME(STYPE v) {                 \
 }
 
 #define SETZERO(VTYPE, NAME)                                       \
-static FORCEINLINE VTYPE __setzero_##NAME() {                      \
+template <class RetVecType> VTYPE __setzero_##NAME();              \
+template <> FORCEINLINE VTYPE __setzero_##NAME<VTYPE>() {               \
     VTYPE ret;                                                     \
     for (int i = 0; i < 64; ++i)                                   \
         ret.v[i] = 0;                                              \
@@ -477,7 +479,8 @@ static FORCEINLINE VTYPE __setzero_##NAME() {                      \
 }
 
 #define UNDEF(VTYPE, NAME)                                         \
-static FORCEINLINE VTYPE __undef_##NAME(VTYPE retType) {           \
+template <class RetVecType> VTYPE __undef_##NAME();                \
+template <> FORCEINLINE VTYPE __undef_##NAME<VTYPE>() {                 \
     return VTYPE();                                                \
 }
 
@@ -606,7 +609,8 @@ template <int ALIGN> static FORCEINLINE void __store(__vec64_i1 *p, __vec64_i1 v
     *ptr = v.v;
 }
 
-static FORCEINLINE __vec64_i1 __smear_i1(int v) {
+template <class RetVecType> __vec64_i1 __smear_i1(int i);
+template <> FORCEINLINE __vec64_i1 __smear_i1<__vec64_i1>(int v) {
     return __vec64_i1(v, v, v, v, v, v, v, v, 
                       v, v, v, v, v, v, v, v,
                       v, v, v, v, v, v, v, v,
@@ -617,7 +621,8 @@ static FORCEINLINE __vec64_i1 __smear_i1(int v) {
                       v, v, v, v, v, v, v, v);
 }
 
-static FORCEINLINE __vec64_i1 __setzero_i1() {
+template <class RetVecType> __vec64_i1 __setzero_i1();
+template <> FORCEINLINE __vec64_i1 __setzero_i1<__vec64_i1>() {
     return __vec64_i1(0, 0, 0, 0, 0, 0, 0, 0, 
                       0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0,
@@ -628,7 +633,8 @@ static FORCEINLINE __vec64_i1 __setzero_i1() {
                       0, 0, 0, 0, 0, 0, 0, 0);
 }
 
-static FORCEINLINE __vec64_i1 __undef_i1() {
+template <class RetVecType> __vec64_i1 __undef_i1();
+template <> FORCEINLINE __vec64_i1 __undef_i1<__vec64_i1>() {
     return __vec64_i1();
 }
 

examples/intrinsics/knc.h
@@ -477,15 +477,18 @@ template <int ALIGN> static FORCEINLINE void __store(__vec16_i1 *p, __vec16_i1 v
     *ptr = v.m;
 }
 
-static FORCEINLINE __vec16_i1 __smear_i1(int i) {
+template <class RetVecType> __vec16_i1 __smear_i1(int i);
+template <> static FORCEINLINE __vec16_i1 __smear_i1<__vec16_i1>(int i) {
     return i?0xFFFF:0x0;
 }
 
-static FORCEINLINE __vec16_i1 __setzero_i1() {
+template <class RetVecType> __vec16_i1 __setzero_i1();
+template <> static FORCEINLINE __vec16_i1 __setzero_i1<__vec16_i1>() {
     return 0;
 }
 
-static FORCEINLINE __vec16_i1 __undef_i1() {
+template <class RetVecType> __vec16_i1 __undef_i1();
+template <> static FORCEINLINE __vec16_i1 __undef_i1<__vec16_i1>() {
     return __vec16_i1(); // FIXME? __mm512_undef_mask();
 }
 
@@ -744,15 +747,18 @@ static FORCEINLINE __vec16_i32 __select(bool cond, __vec16_i32 a, __vec16_i32 b)
 static FORCEINLINE int32_t __extract_element(__vec16_i32 v, int index) { return ((int32_t *)&v)[index]; }
 static FORCEINLINE void __insert_element(__vec16_i32 *v, int index, int32_t val) { ((int32_t *)v)[index] = val; }
 
-static FORCEINLINE __vec16_i32 __smear_i32(int32_t i) {
+template <class RetVecType> __vec16_i32 __smear_i32(int32_t i);
+template <> static FORCEINLINE __vec16_i32 __smear_i32<__vec16_i32>(int32_t i) {
     return _mm512_set_1to16_epi32(i);
 }
 
-static FORCEINLINE __vec16_i32 __setzero_i32() {
+template <class RetVecType> __vec16_i32 __setzero_i32();
+template <> static FORCEINLINE __vec16_i32 __setzero_i32<__vec16_i32>() {
     return _mm512_setzero_epi32();
 }
 
-static FORCEINLINE __vec16_i32 __undef_i32() {
+template <class RetVecType> __vec16_i32 __undef_i32();
+template <> static FORCEINLINE __vec16_i32 __undef_i32<__vec16_i32>() {
     return _mm512_undefined_epi32();
 }
 
@@ -803,15 +809,24 @@ template <> static FORCEINLINE void __store<64>(__vec16_i32 *p, __vec16_i32 v) {
 // int64
 
 
-static FORCEINLINE __vec16_i64 __setzero_i64() {
+template <class RetVecType> __vec16_i64 __setzero_i64();
+template <> static FORCEINLINE __vec16_i64 __setzero_i64<__vec16_i64>() {
     __vec16_i64 ret;
     ret.v_lo = _mm512_setzero_epi32();
     ret.v_hi = _mm512_setzero_epi32();
     return ret;
 }
 
+template <class RetVecType> __vec16_i64 __undef_i64();
+template <> static FORCEINLINE __vec16_i64 __undef_i64<__vec16_i64>() {
+    __vec16_i64 ret;
+    ret.v_lo = _mm512_undefined_epi32();
+    ret.v_hi = _mm512_undefined_epi32();
+    return ret;
+}
+
 static FORCEINLINE __vec16_i64 __add(const __vec16_i64 &a, const __vec16_i64 &b)
-{
+{    
     __mmask16 carry = 0;
     __m512i lo = _mm512_addsetc_epi32(a.v_lo, b.v_lo, &carry);
     __m512i hi = _mm512_adc_epi32(a.v_hi, carry, b.v_hi, &carry);
@@ -885,7 +900,8 @@ static FORCEINLINE int64_t __extract_element(const __vec16_i64 &v, int index)
     return src[index+16] | (int64_t(src[index]) << 32);
 }
 
-static FORCEINLINE  __vec16_i64 __smear_i64(const int64_t &l) {
+template <class RetVecType> __vec16_i64 __smear_i64(const int64_t &l);
+template <> FORCEINLINE  __vec16_i64 __smear_i64<__vec16_i64>(const int64_t &l) {
     const int *i = (const int*)&l;
     return __vec16_i64(_mm512_set_1to16_epi32(i[0]), _mm512_set_1to16_epi32(i[1]));
 }
@@ -897,12 +913,12 @@ LOAD_STORE(__vec16_i64, int64_t)
 
 
 template <int ALIGN> static FORCEINLINE __vec16_i64 __load(const __vec16_i64 *p) {
-    __m512i v1;
-    __m512i v2;
-    v2 = _mm512_extloadunpackhi_epi32(v1, p, _MM_UPCONV_EPI32_NONE, _MM_HINT_NONE);
-    v2 = _mm512_extloadunpacklo_epi32(v1, p, _MM_UPCONV_EPI32_NONE, _MM_HINT_NONE);
-    v1 = _mm512_extloadunpackhi_epi32(v2, ((uint8_t*)p)+64, _MM_UPCONV_EPI32_NONE, _MM_HINT_NONE);
-    v1 = _mm512_extloadunpacklo_epi32(v2, ((uint8_t*)p)+64, _MM_UPCONV_EPI32_NONE, _MM_HINT_NONE);
+    __vec16_i32 v1 = _mm512_undefined_epi32();
+    __vec16_i32 v2 = _mm512_undefined_epi32();
+    v2 = _mm512_extloadunpackhi_epi32(v2, p, _MM_UPCONV_EPI32_NONE, _MM_HINT_NONE);
+    v2 = _mm512_extloadunpacklo_epi32(v2, p, _MM_UPCONV_EPI32_NONE, _MM_HINT_NONE);
+    v1 = _mm512_extloadunpackhi_epi32(v1, ((uint8_t*)p)+64, _MM_UPCONV_EPI32_NONE, _MM_HINT_NONE);
+    v1 = _mm512_extloadunpacklo_epi32(v1, ((uint8_t*)p)+64, _MM_UPCONV_EPI32_NONE, _MM_HINT_NONE);
 
     __vec16_i64 ret;
     ret.v_hi = _mm512_mask_permutevar_epi32(ret.v_hi, 0xFF00,
@@ -1078,15 +1094,18 @@ static FORCEINLINE void  __insert_element(__vec16_f *v, int index, float val) {
     ((float *)v)[index] = val;
 }
 
-static FORCEINLINE __vec16_f __smear_float(float f) {
+template <class RetVecType> __vec16_f __smear_float(float f);
+template <> static FORCEINLINE __vec16_f __smear_float<__vec16_f>(float f) {
     return _mm512_set_1to16_ps(f);
 }
 
-static FORCEINLINE __vec16_f __setzero_float() {
+template <class RetVecType> __vec16_f __setzero_float();
+template <> static FORCEINLINE __vec16_f __setzero_float<__vec16_f>() {
     return _mm512_setzero_ps();
 }
 
-static FORCEINLINE __vec16_f __undef_float() {
+template <class RetVecType> __vec16_f __undef_float();
+template <> static FORCEINLINE __vec16_f __undef_float<__vec16_f>() {
     return _mm512_undefined_ps();
 }
 
@@ -1287,21 +1306,24 @@ static FORCEINLINE void  __insert_element(__vec16_d *v, int index, double val) {
     ((double *)v)[index] = val;
 }
 
-static FORCEINLINE __vec16_d __smear_double(double d) {
+template <class RetVecType> __vec16_d __smear_double(double d);
+template <> static FORCEINLINE __vec16_d __smear_double<__vec16_d>(double d) {
     __vec16_d ret;
     ret.v1 = _mm512_extload_pd(&d, _MM_UPCONV_PD_NONE, _MM_BROADCAST_1X8, _MM_HINT_NONE);
     ret.v2 = ret.v1;
     return ret;
 }
 
-static FORCEINLINE __vec16_d __setzero_double() {
+template <class RetVecType> __vec16_d __setzero_double();
+template <> static FORCEINLINE __vec16_d __setzero_double<__vec16_d>() {
     __vec16_d ret;
     ret.v1 = _mm512_setzero_pd();
     ret.v2 = ret.v1;
     return ret;
 }
 
-static FORCEINLINE __vec16_d __undef_double() {
+template <class RetVecType> __vec16_d __undef_double();
+template <> static FORCEINLINE __vec16_d __undef_double<__vec16_d>() {
     __vec16_d ret;
     ret.v1 = _mm512_undefined_pd();
     ret.v2 = ret.v1;

commit fe6282e837eb833dc2459edcfe4fd4bef42b856e

Fixed small issue with name mangling introduced in aecd6e08

cbackend.cpp
@@ -49,7 +49,7 @@
 #include "llvm/Analysis/ValueTracking.h"
 #include "llvm/CodeGen/Passes.h"
 #include "llvm/CodeGen/IntrinsicLowering.h"
-#include "llvm/Target/Mangler.h"
+//#include "llvm/Target/Mangler.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/MC/MCAsmInfo.h"
 #include "llvm/MC/MCContext.h"
@@ -218,7 +218,7 @@ namespace {
   class CWriter : public llvm::FunctionPass, public llvm::InstVisitor<CWriter> {
     llvm::formatted_raw_ostream &Out;
     llvm::IntrinsicLowering *IL;
-    llvm::Mangler *Mang;
+    //llvm::Mangler *Mang;
     llvm::LoopInfo *LI;
     const llvm::Module *TheModule;
     const llvm::MCAsmInfo* TAsm;
@@ -251,7 +251,7 @@ namespace {
     static char ID;
       explicit CWriter(llvm::formatted_raw_ostream &o, const char *incname,
                        int vecwidth)
-      : FunctionPass(ID), Out(o), IL(0), Mang(0), LI(0),
+          : FunctionPass(ID), Out(o), IL(0), /* Mang(0), */ LI(0),
         TheModule(0), TAsm(0), MRI(0), MOFI(0), TCtx(0), TD(0),
         OpaqueCounter(0), NextAnonValueNumber(0), 
         includeName(incname ? incname : "generic_defs.h"),
@@ -298,7 +298,7 @@ namespace {
       // Free memory...
       delete IL;
       delete TD;
-      delete Mang;
+      //delete Mang;
       delete TCtx;
       delete TAsm;
       delete MRI;
@@ -1726,9 +1726,11 @@ std::string CWriter::GetValueName(const llvm::Value *Operand) {
 
   // Mangle globals with the standard mangler interface for LLC compatibility.
   if (const llvm::GlobalValue *GV = llvm::dyn_cast<llvm::GlobalValue>(Operand)) {
-    llvm::SmallString<128> Str;
-    Mang->getNameWithPrefix(Str, GV, false);
-    return CBEMangle(Str.str().str());
+    (void)GV;
+    //llvm::SmallString<128> Str;
+    //Mang->getNameWithPrefix(Str, GV, false);
+    //return CBEMangle(Str.str().str());
+    return CBEMangle(Operand->getName().str().c_str());
   }
 
   std::string Name = Operand->getName();
@@ -2149,7 +2151,7 @@ bool CWriter::doInitialization(llvm::Module &M) {
   TAsm = new CBEMCAsmInfo();
   MRI  = new llvm::MCRegisterInfo();
   TCtx = new llvm::MCContext(*TAsm, *MRI, NULL);
-  Mang = new llvm::Mangler(*TCtx, *TD);
+  //Mang = new llvm::Mangler(*TCtx, *TD);
 
   // Keep track of which functions are static ctors/dtors so they can have
   // an attribute added to their prototypes.