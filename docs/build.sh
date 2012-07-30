63ed90b0fd3e6aa58628ba8fa7ac686915f48087
Jean-Luc Duprat <jld@acm.org>
2012-07-30 11:47:25 -0700

    docs/build.sh runs rst2html rather than rst2html.py Explicitly documented that fact that ICC needs the -mmic flag to compile for KNC. Updated ISPC User Guide with details on ICC compiler options that impact FP performance in generated code.

--- docs/build.sh
+++ docs/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 for i in ispc perfguide faq; do
-    rst2html.py --template=template.txt --link-stylesheet \
+    rst2html --template=template.txt --link-stylesheet \
         --stylesheet-path=css/style.css $i.rst > $i.html
 done
 
-rst2html.py --template=template-news.txt --link-stylesheet \
+rst2html --template=template-news.txt --link-stylesheet \
     --stylesheet-path=css/style.css news.rst > news.html
 
-rst2html.py --template=template-perf.txt --link-stylesheet \
+rst2html --template=template-perf.txt --link-stylesheet \
         --stylesheet-path=css/style.css perf.rst > perf.html
 
 #rst2latex --section-numbering --documentclass=article --documentoptions=DIV=9,10pt,letterpaper ispc.txt > ispc.tex
