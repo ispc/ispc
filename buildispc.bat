0ce67f37ac333c4b62a676f5c0058923cbba2708
Matt Pharr <matt.pharr@intel.com>
2012-09-06 06:04:32 -0700

    Use LLVM_VERSION env variable to get LLVM version with MSVC build.
    Previously, it was set directly in the ispc.vcxproj file.
    
    Issue #371.

--- buildispc.bat
+++ buildispc.bat
@@ -2,8 +2,8 @@
 
 REM If LLVM_INSTALL_DIR isn't set globally in your environment,
 REM it can be set here_
-set LLVM_INSTALL_DIR=c:\users\mmp\llvm-dev
-set LLVM_VERSION=3.2
+REM set LLVM_INSTALL_DIR=c:\users\mmp\llvm-dev
+REM set LLVM_VERSION=3.2
 
 REM Both the LLVM binaries and python need to be in the path
 set path=%LLVM_INSTALL_DIR%\bin;%PATH%;c:\cygwin\bin
