99df2d9dbff4ba44bb0dcee5984b20697c4321a9
Dmitry Babokin <babokin@gmail.com>
2013-10-09 14:05:19 +0400

    Switch examples on Unix from using g++ to clang++

--- examples/common.mk
+++ examples/common.mk
@@ -3,14 +3,14 @@ TASK_CXX=../tasksys.cpp
 TASK_LIB=-lpthread
 TASK_OBJ=objs/tasksys.o
 
-CXX=g++
-CXXFLAGS=-Iobjs/ -O2
-CC=gcc
-CCFLAGS=-Iobjs/ -O2
+CXX=clang++
+CXXFLAGS+=-Iobjs/ -O2
+CC=clang
+CCFLAGS+=-Iobjs/ -O2
 
 LIBS=-lm $(TASK_LIB) -lstdc++
 ISPC=ispc
-ISPC_FLAGS=-O2
+ISPC_FLAGS+=-O2
 ISPC_HEADER=objs/$(ISPC_SRC:.ispc=_ispc.h)
 
 ARCH:=$(shell uname -m | sed -e s/x86_64/x86/ -e s/i686/x86/ -e s/arm.*/arm/ -e s/sa110/arm/)
