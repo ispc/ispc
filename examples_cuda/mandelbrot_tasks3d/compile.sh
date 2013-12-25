c3165df156194f526c5a24a56669200a5a58c577
Evghenii <egaburov@dds.nl>
2013-12-25 23:33:48 +0100

    +1

--- examples_cuda/mandelbrot_tasks3d/compile.sh
+++ examples_cuda/mandelbrot_tasks3d/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ptxas -arch=sm_35 -c -o kernel.gpu.o kernel_cu.ptx       
 fatbinary -arch=sm_35 -create kernel.fatbin -elf kernel.gpu.o 
-nvcc -arch=sm_35 -Xptxas="-v" -dlink -o mandel_cu.o kernel.fatbin kernel_driver.cu  -rdc=true -lcudadevrt
+nvcc -arch=sm_35 -Xptxas="-v" -dc  kernel_driver.cu   -lcudadevrt
+nvcc -arch=sm_35 -Xptxas="-v" -dlink -o mandel_nvcc.o kernel.fatbin kernel_driver.o  -rdc=true -lcudadevrt
 
