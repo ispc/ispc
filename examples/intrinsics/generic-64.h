688d9c9a820c3fc8e34fe833e0fde16db8a62b12
Evghenii <egaburov@dds.nl>
2014-02-05 13:20:44 +0100

    added support for rsqrtd/rcpd for generic-*.h

--- examples/intrinsics/generic-64.h
+++ examples/intrinsics/generic-64.h
@@ -1271,8 +1271,16 @@ static FORCEINLINE float __rsqrt_uniform_float(float v) {
     return 1.f / sqrtf(v);
 }
 
+static FORCEINLINE double __rsqrt_uniform_double(double v) {
+    return 1.0 / sqrt(v);
+}
+
 static FORCEINLINE float __rcp_uniform_float(float v) {
     return 1.f / v;
+}
+
+static FORCEINLINE double __rcp_uniform_double(double v) {
+    return 1.0 / v;
 }
 
 static FORCEINLINE float __sqrt_uniform_float(float v) {
@@ -1284,7 +1292,9 @@ static FORCEINLINE double __sqrt_uniform_double(double v) {
 }
 
 UNARY_OP(__vec64_f, __rcp_varying_float, __rcp_uniform_float)
+UNARY_OP(__vec64_d, __rcp_varying_double, __rcp_uniform_double)
 UNARY_OP(__vec64_f, __rsqrt_varying_float, __rsqrt_uniform_float)
+UNARY_OP(__vec64_d, __rsqrt_varying_double, __rsqrt_uniform_double)
 UNARY_OP(__vec64_f, __sqrt_varying_float, __sqrt_uniform_float)
 UNARY_OP(__vec64_d, __sqrt_varying_double, __sqrt_uniform_double)
 
