96cb1ebb3a576d00c9f1aa0b0022f7d517b045f0
Lukas Böhm <suluke93@gmail.com>
2017-06-12 17:43:02 +0200

    Fix for llvm upstream change
    Relevant svn commit: 304864
    Github: https://github.com/llvm-mirror/llvm/commit/19ca2b0f9daed883c21730285d7f04424e5f5f88

--- ispc.cpp
+++ ispc.cpp
@@ -72,7 +72,11 @@
   #include <llvm/DebugInfo.h>
   #include <llvm/DIBuilder.h>
 #endif
-#include <llvm/Support/Dwarf.h>
+#if ISPC_LLVM_VERSION >= ISPC_LLVM_5_0 // LLVM 5.0+
+    #include <llvm/BinaryFormat/Dwarf.h>
+#else // LLVM up to 4.x
+    #include <llvm/Support/Dwarf.h>
+#endif
 #include <llvm/Target/TargetMachine.h>
 #include <llvm/Target/TargetOptions.h>
 #if ISPC_LLVM_VERSION == ISPC_LLVM_3_2
