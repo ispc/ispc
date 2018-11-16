71b24470efc505880405e1fc5d34d234d16569c3
Deepak Rajendrakumaran <deepak.rajendrakumaran@intel.com>
2018-11-16 11:46:03 -0800

    Fixing compiler warnings + trailing whitespace.

--- examples/gmres/mmio.h
+++ examples/gmres/mmio.h
@@ -127,9 +127,6 @@ int mm_read_mtx_crd_data(FILE *f, int M, int N, int nz, int I[], int J[],
 int mm_read_mtx_crd_entry(FILE *f, int *I, int *J, double *real, double *img,
 			MM_typecode matcode);
 
-int mm_read_unsymmetric_sparse(const char *fname, int *M_, int *N_, int *nz_,
-                double **val_, int **I_, int **J_);
-
 
 
 #endif
