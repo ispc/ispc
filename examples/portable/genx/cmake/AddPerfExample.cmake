a1435a6bb971db0e10e32e9537b71adb227fe401
Petr Kurapov <petr.a.kurapov@intel.com>
2019-10-17 16:30:36 +0300

    Added GenX examples

---
+++ examples/portable/genx/cmake/AddPerfExample.cmake
@@ -0,0 +1,102 @@
+#
+#  Copyright (c) 2019, Intel Corporation
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are
+#  met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of Intel Corporation nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+#   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+#   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+#   PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+#   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+#   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+#   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+message(STATUS "Processing cmake/AddPerfExample.cmake")
+function(add_perf_example)
+    set(options CM_TEST)
+    set(oneValueArgs NAME ISPC_SRC_NAME CM_SRC_NAME ISPC_OBJ_NAME HOST_NAME CM_HOST_NAME CM_OBJ_NAME TEST_NAME)
+    set(multiValueArgs ISPC_FLAGS HOST_SOURCES CM_HOST_SOURCES CM_HOST_FLAGS)
+    cmake_parse_arguments("parsed" "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+# compile ispc kernel    
+    list(APPEND ISPC_BUILD_OUTPUT ${parsed_ISPC_OBJ_NAME})
+    add_custom_command(
+                       OUTPUT ${ISPC_BUILD_OUTPUT}
+                       COMMAND ${ISPC_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/${parsed_ISPC_SRC_NAME}.ispc ${parsed_ISPC_FLAGS} --target=genx -o ${parsed_ISPC_OBJ_NAME}
+                       VERBATIM
+                       DEPENDS ${ISPC_EXECUTABLE}
+                       DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${parsed_ISPC_SRC_NAME}.ispc"
+                      )
+    add_executable(${parsed_TEST_NAME} ${ISPC_BUILD_OUTPUT} "${CMAKE_CURRENT_SOURCE_DIR}/${parsed_ISPC_SRC_NAME}.ispc") 
+    set_target_properties(${parsed_TEST_NAME} PROPERTIES LINKER_LANGUAGE CXX)
+    target_sources(${parsed_TEST_NAME} PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/${parsed_ISPC_SRC_NAME}.ispc")
+
+# compile host code
+    set(HOST_EXECUTABLE ${parsed_HOST_NAME}_${parsed_TEST_NAME})
+    add_executable(${HOST_EXECUTABLE} ${parsed_HOST_SOURCES})
+    if (WIN32)
+        target_include_directories(${HOST_EXECUTABLE} PRIVATE "${COMMON_PATH}"
+                                   "${MDF}/compiler/include_icl"
+                                   "${MDF}/compiler/include_icl/cm"
+                                   "${MDF}/runtime/include"
+                                   "${MDF}/examples/helper")
+
+        target_link_libraries(${HOST_EXECUTABLE} "${MDF}/runtime/lib/x86/igfx11cmrt32.lib"
+                              "${MDF}/compiler/lib/x86/libcm.lib" AdvAPI32 Ole32)
+    endif()
+    install(TARGETS "${HOST_EXECUTABLE}" RUNTIME DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
+        PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+# compile cm kernel if present
+    if (parsed_CM_TEST)
+        set(CM_TEST_NAME "${parsed_TEST_NAME}_cm")
+        list(APPEND CM_BUILD_OUTPUT ${parsed_CM_OBJ_NAME})
+
+        if (WIN32)
+            set(CMC_SUPPORT_INCLUDE ${CMC_INSTALL_PATH}/include)
+        endif()
+
+        add_custom_target(${parsed_CM_SRC_NAME} ALL
+            COMMAND ${CM_EXECUTABLE} -isystem ${CMC_SUPPORT_INCLUDE} -march=SKL "/DCM_PTRSIZE=32" ${CMAKE_CURRENT_SOURCE_DIR}/${parsed_CM_SRC_NAME}.cpp -o ${parsed_CM_OBJ_NAME}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+            VERBATIM
+            DEPENDS ${CM_EXECUTABLE}
+            DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${parsed_CM_SRC_NAME}.cpp
+        )
+
+        set(CM_HOST_EXECUTABLE ${parsed_CM_HOST_NAME}_${parsed_TEST_NAME})
+        add_executable(${CM_HOST_EXECUTABLE} ${parsed_CM_HOST_SOURCES})
+        if (WIN32)
+            target_include_directories(${CM_HOST_EXECUTABLE} PRIVATE "${COMMON_PATH}"
+                                       "${MDF}/compiler/include_icl"
+                                       "${MDF}/compiler/include_icl/cm"
+                                       "${MDF}/runtime/include"
+                                       "${MDF}/examples/helper")
+
+            target_link_libraries(${CM_HOST_EXECUTABLE} "${MDF}/runtime/lib/x86/igfx11cmrt32.lib"
+                                  "${MDF}/compiler/lib/x86/libcm.lib" AdvAPI32 Ole32)
+        endif()
+        target_compile_options(${CM_HOST_EXECUTABLE} PRIVATE ${parsed_CM_HOST_FLAGS})
+        install(TARGETS "${CM_HOST_EXECUTABLE}" RUNTIME DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
+            PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+
+    endif()
+
+endfunction()
---
+++ examples/portable/genx/sgemm/NaiveCMSgemm_genx.cpp
@@ -0,0 +1,111 @@
+/*
+  Copyright (c) 2019, Intel Corporation
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+    * Neither the name of Intel Corporation nor the names of its
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
+
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+   PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#define GEN_KERNEL
+#undef CM_DEBUG
+
+#include "cm/cm.h"
+
+// alpha = 1; beta = 0;
+const short init_0_15[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+extern "C" _GENX_MAIN_ void sgemm_kernel(svmptr_t indxA, svmptr_t indxB, svmptr_t indxC, int M, int N, int K) {
+    unsigned int x = cm_group_id(0) * cm_local_size(0) + cm_local_id(0);
+    unsigned int y = cm_group_id(1) * cm_local_size(1) + cm_local_id(1);
+
+    unsigned int threadNum = cm_linear_global_size();
+    unsigned int threadid = cm_linear_global_id();
+
+    // doesn't work for threadNum > K
+    unsigned int rowPerTask = M / cm_linear_global_size();
+
+    unsigned int start = threadid * rowPerTask;
+    unsigned int end = start + rowPerTask;
+
+    /*printf("%u Thread says rowPerTask is %u: start=%u;end=%u\n", threadid, rowPerTask, start, end);*/
+
+    const int simd_size = 16;
+    vector<float, simd_size> a, c; // assuming this is 2 consequent registers
+    vector<float, 1> b;
+
+    vector<uint, simd_size> off(init_0_15);
+    vector<svmptr_t, simd_size> offseta;
+    vector<svmptr_t, 1> offsetb;
+    vector<svmptr_t, simd_size> offsetc;
+
+    unsigned int m = start;
+    for (; m < end; m += simd_size) {
+        // don't let simd overlap
+        if (m + simd_size > end) {
+            break;
+        }
+        for (int k = 0; k < K; k++) {
+            c = 0;
+            for (int n = 0; n < N; n++) {
+                offseta = indxA + (m * N + n) * sizeof(float) + off * N * sizeof(float);
+                offsetb = indxB + (n * K + k) * sizeof(float);
+                cm_svm_scatter_read(offseta, a);
+                cm_svm_scatter_read(offsetb, b);
+
+                c += a * b[0];
+            }
+            offsetc = indxC + (m * K + k) * sizeof(float) + off * K * (sizeof(float));
+            // printf("%u Thread says OFFSET[%u]\n", threadid, indxC + (k*M + m) * sizeof(float));
+            cm_svm_scatter_write(offsetc, c);
+        }
+    }
+    // printf("%u thread says: m=%u end=%u\n", threadid, m, end);
+
+    if (m != end) {
+        // remainder loop
+        off += m;
+        SIMD_IF_BEGIN(off >= end) { off = end - 1; }
+        SIMD_IF_END;
+
+        vector<float, simd_size> a_rem, c_rem;
+        vector<svmptr_t, simd_size> a_rem_offset, c_rem_offset;
+        vector<svmptr_t, 1> b_rem_offset;
+        for (int k = 0; k < K; k++) {
+            c_rem = 0;
+            for (int n = 0; n < N; n++) {
+                a_rem_offset = indxA + (off * N + n) * sizeof(float);
+                b_rem_offset = indxB + (n * K + k) * sizeof(float);
+                cm_svm_scatter_read(a_rem_offset, a_rem);
+                cm_svm_scatter_read(b_rem_offset, b);
+                c_rem += a_rem * b[0];
+            }
+            c_rem_offset = indxC + (off * K + k) * sizeof(float);
+            cm_svm_scatter_write(c_rem_offset, c_rem);
+        }
+    }
+}
