177198dbec87a908a36d36d59432370fa968f13c
Dmitry Babokin <dmitry.y.babokin@intel.com>
2020-01-29 16:14:51 -0800

    Updating copyright

--- examples/cmake/AddISPCExample.cmake
+++ examples/cmake/AddISPCExample.cmake
@@ -1,5 +1,5 @@
 #
-#  Copyright (c) 2018-2019, Intel Corporation
+#  Copyright (c) 2018-2020, Intel Corporation
 #  All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
