f51091d72917fc9217550fea75400c29f08b46e8
Anton Schreiner <antonschrein@gmail.com>
2020-04-20 13:40:03 +0000

    Try installing V8 with jsvu

--- scripts/install_v8.sh
+++ scripts/install_v8.sh
@@ -1,12 +1,7 @@
 WD=`pwd`
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH=$WD/depot_tools:"$PATH"
-fetch v8 &&  \
-cd v8 &&  \
-gclient sync &&  \
-./build/install-build-deps.sh &&  \
-./tools/dev/v8gen.py x64.release &&  \
-ninja -j12 -C out.gn/x64.release &&  \
-export PATH=$WD/v8/out.gn/x64.release:"$PATH"
-
+sudo apt install nodejs && \
+npm install jsvu -g && \
+export PATH="${HOME}/.jsvu:${PATH}" && \
+jsvu --os=linux64 --engines=v8-debug && \
+alias d8=v8-debug
 cd $WD
