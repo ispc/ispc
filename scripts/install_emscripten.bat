461c5a1a121abd962c132f6520c16347189009b5
Dmitry Babokin <dmitry.y.babokin@intel.com>
2020-05-06 09:50:30 -0700

    Fix emcc version in line with #1750

--- scripts/install_emscripten.bat
+++ scripts/install_emscripten.bat
@@ -2,6 +2,6 @@ set WD=%cd%
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
 git pull
-call emsdk.bat install latest
-call emsdk.bat activate latest
+call emsdk.bat install sdk-1.39.11-64bit
+call emsdk.bat activate sdk-1.39.11-64bit
 cd %WD%
