aa3cfa5f225f3546fdb7488499c1925b8dbb1e3a
Anton Schreiner <antonschrein@gmail.com>
2020-05-06 12:34:16 +0000

    Uncomment jsvu installation

--- scripts/install_v8.sh
+++ scripts/install_v8.sh
@@ -1,6 +1,6 @@
 WD=`pwd`
-#sudo apt-get install nodejs && \
-#npm install jsvu -g && \
+sudo apt-get install nodejs && \
+npm install jsvu -g && \
 export PATH="${HOME}/.jsvu:${PATH}" && \
 jsvu --os=linux64 v8-debug@8.4.200 && \
 cp scripts/v8-redirect.sh ${HOME}/.jsvu/v8 && \
