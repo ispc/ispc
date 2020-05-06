54095c1a96930e73a20e68e19bad262a7c1fc14a
Anton Schreiner <antonschrein@gmail.com>
2020-05-06 12:12:42 +0000

    [WASM] Enable tests Set versions of  emscripten and v8 to the recent compatible

--- scripts/install_v8.sh
+++ scripts/install_v8.sh
@@ -1,6 +1,8 @@
 WD=`pwd`
-sudo apt-get install nodejs && \
-npm install jsvu -g && \
+#sudo apt-get install nodejs && \
+#npm install jsvu -g && \
 export PATH="${HOME}/.jsvu:${PATH}" && \
-jsvu --os=linux64 --engines=v8-debug
+jsvu --os=linux64 v8-debug@8.4.200 && \
+cp scripts/v8-redirect.sh ${HOME}/.jsvu/v8 && \
+chmod +x ${HOME}/.jsvu/v8
 cd $WD
