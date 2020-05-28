ef4f3ac5987382ab0bb954fad012b478077e794d
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2020-05-28 13:27:57 -0700

    Switched aobench to ispcrt

--- examples/portable/genx/aobench/ao.cpp
+++ examples/portable/genx/aobench/ao.cpp
@@ -57,12 +57,22 @@
 #include <string>
 #include <sys/types.h>
 
+// ispcrt
+#include "ispcrt.h"
+
 #define NSUBSAMPLES 2
 #define CORRECTNESS_THRESHOLD 0.01
 
 using namespace hostutil;
 
 extern void ao_serial(int w, int h, int nsubsamples, float image[]);
+
+struct Parameters {
+    int width;
+    int height;
+    int nsubsamples{NSUBSAMPLES};
+    float *image;
+};
 
 static unsigned int niterations;
 static unsigned int width, height;
@@ -105,137 +115,90 @@ static void savePPM(const char *fname, int w, int h, float *fimg) {
 
 static int run() {
     std::cout.setf(std::ios::unitbuf);
-    int nSubSamples = NSUBSAMPLES;
-    img = new unsigned char[width * height * 3];
-    fimg = (float *)aligned_alloc(4096, width * height * 3 * sizeof(float));
+    ispcrtSetErrorFunc([](ISPCRTError e, const char *m) {
+        std::cerr << "ISPCRT Error! --> " << m << std::endl;
+        std::exit(1);
+    });
 
-    // L0 initialization
-    ze_device_handle_t hDevice = nullptr;
-    ze_module_handle_t hModule = nullptr;
-    ze_driver_handle_t hDriver = nullptr;
-    ze_command_queue_handle_t hCommandQueue = nullptr;
-    L0InitContext(hDriver, hDevice, hModule, hCommandQueue, "aobench_ispc.spv");
+    size_t imgSize = width * height * 3;
+    img = new unsigned char[imgSize];
+    fimg = (float *)aligned_alloc(4096, imgSize * sizeof(float));
 
-    ze_command_list_handle_t hCommandList;
-    ze_kernel_handle_t hKernel;
-    L0Create_Kernel(hDevice, hModule, hCommandList, hKernel, "ao_ispc");
-    //
-    // Run the ispc gpu code nIterations times, and report the minimum
-    // time for any of them.
-    //
-    double minCyclesISPCGPU = 1e30;
     uint64_t minKernelClocksGPU = std::numeric_limits<uint64_t>::max();
-    void *dFimg = nullptr;
 
-    // Profiling initialization
-    ze_device_properties_t device_properties;
-    L0_SAFE_CALL(zeDeviceGetProperties(hDevice, &device_properties));
-    uint64_t timestampFreq = device_properties.timerResolution;
+    // Init compute device (CPU or GPU)
+    auto run_kernel = [&](ISPCRTDeviceType type) {
+        auto device = ispcrtGetDevice(type);
 
-    // Create event pool
-    ze_event_pool_desc_t tsEventPoolDesc;
-    tsEventPoolDesc.count = 1;
-    tsEventPoolDesc.flags = ZE_EVENT_POOL_FLAG_TIMESTAMP;
-    tsEventPoolDesc.version = ZE_EVENT_POOL_DESC_VERSION_CURRENT;
+        // Setup output array
+        auto buf_dev = ispcrtNewMemoryView(device, fimg, imgSize * sizeof(float));
 
-    ze_event_pool_handle_t hTSEventPool;
-    L0_SAFE_CALL(zeEventPoolCreate(hDriver, &tsEventPoolDesc, 1, &hDevice, &hTSEventPool));
+        // Setup parameters structure
+        Parameters p;
+        p.width = width;
+        p.height = height;
+        p.image = (float *)ispcrtDevicePtr(buf_dev);
 
-    ze_event_desc_t tsEventDesc;
-    tsEventDesc.index = 0;
-    tsEventDesc.signal = ZE_EVENT_SCOPE_FLAG_HOST;
-    tsEventDesc.wait = ZE_EVENT_SCOPE_FLAG_HOST;
-    tsEventDesc.version = ZE_EVENT_DESC_VERSION_CURRENT;
+        auto p_dev = ispcrtNewMemoryView(device, &p, sizeof(p));
 
-    ze_event_handle_t hTSEvent;
-    L0_SAFE_CALL(zeEventCreate(hTSEventPool, &tsEventDesc, &hTSEvent));
+        // Create module and kernel to execute
+        auto module = ispcrtLoadModule(device, "aobench");
+        auto kernel = ispcrtNewKernel(device, module, "ao_ispc");
+        // Create task queue and execute kernel
+        auto queue = ispcrtNewTaskQueue(device);
 
-    // thread space
-    ze_group_count_t dispatchTraits = {1, height*width/16, 1};
-    std::cout << "Set dispatchTraits.x=" << dispatchTraits.groupCountX
-              << ", dispatchTraits.y=" << dispatchTraits.groupCountY << std::endl;
+        // Run the ispc gpu code nIterations times, and report the minimum
+        // time for any of them.
+        const char *device_str = (type == ISPCRT_DEVICE_TYPE_GPU) ? "GPU" : "CPU";
+        double minCyclesISPC = 1e30;
+        for (unsigned int i = 0; i < niterations; i++) {
+            memset((void *)fimg, 0, sizeof(float) * width * height * 3);
+            ispcrtCopyToDevice(queue, p_dev);
+            ispcrtDeviceBarrier(queue);
+            ispcrtSync(queue);
+            reset_and_start_timer();
+            auto wct = std::chrono::system_clock::now();
+            ispcrtLaunch2D(queue, kernel, p_dev, height * width / 16, 1);
+            ispcrtDeviceBarrier(queue);
+            ispcrtSync(queue);
+            double dt = get_elapsed_mcycles();
+            auto dur = (std::chrono::system_clock::now() - wct);
+            auto secs = std::chrono::duration_cast<std::chrono::nanoseconds>(dur);
+            printf("@time of %s run:\t\t\t[%ld] nanoseconds\n", device_str, dt);
+            printf("@time of %s run:\t\t\t[%.3f] million cycles\n", device_str, dt);
+            minCyclesISPC = std::min(minCyclesISPC, dt);
+            ispcrtCopyToHost(queue, buf_dev);
+            ispcrtSync(queue);
+        }
 
-    for (unsigned int i = 0; i < niterations; i++) {
-        memset((void *)fimg, 0, sizeof(float) * width * height * 3);
+        printf("[aobench ISPC GPU]:\t\t[%.3f] million cycles (%d x %d image)\n", minCyclesISPC, width, height);
+    };
+    run_kernel(ISPCRT_DEVICE_TYPE_CPU);
+    savePPM("ao-ispc-cpu.ppm", width, height, fimg);
 
-        L0_SAFE_CALL(zeCommandListReset(hCommandList));
-
-        reset_and_start_timer();
-        auto wct = std::chrono::system_clock::now();
-
-        // Alloc & copy
-        ze_device_mem_alloc_desc_t allocDesc = {ZE_DEVICE_MEM_ALLOC_DESC_VERSION_CURRENT,
-                                                ZE_DEVICE_MEM_ALLOC_FLAG_DEFAULT, 0};
-        L0_SAFE_CALL(zeDriverAllocDeviceMem(hDriver, &allocDesc, width * height * 3 * sizeof(float),
-                                            width * height * 3 * sizeof(float), hDevice, &dFimg));
-
-        // copy buffers to device
-        L0_SAFE_CALL(
-            zeCommandListAppendMemoryCopy(hCommandList, dFimg, fimg, width * height * 3 * sizeof(float), nullptr));
-
-        L0_SAFE_CALL(zeCommandListAppendBarrier(hCommandList, nullptr, 0, nullptr));
-
-        // set kernel arguments
-        L0_SAFE_CALL(zeKernelSetArgumentValue(hKernel, 0, sizeof(int), &width));
-        L0_SAFE_CALL(zeKernelSetArgumentValue(hKernel, 1, sizeof(int), &height));
-        L0_SAFE_CALL(zeKernelSetArgumentValue(hKernel, 2, sizeof(int), &nSubSamples));
-        L0_SAFE_CALL(zeKernelSetArgumentValue(hKernel, 3, sizeof(dFimg), &dFimg));
-
-        // launch
-        L0_SAFE_CALL(zeCommandListAppendLaunchKernel(hCommandList, hKernel, &dispatchTraits, hTSEvent, 0, nullptr));
-        L0_SAFE_CALL(zeCommandListAppendBarrier(hCommandList, nullptr, 0, nullptr));
-
-        // copy result to host
-        L0_SAFE_CALL(
-            zeCommandListAppendMemoryCopy(hCommandList, fimg, dFimg, width * height * 3 * sizeof(float), nullptr));
-        L0_SAFE_CALL(zeCommandListClose(hCommandList));
-        L0_SAFE_CALL(zeCommandQueueExecuteCommandLists(hCommandQueue, 1, &hCommandList, nullptr));
-        L0_SAFE_CALL(zeCommandQueueSynchronize(hCommandQueue, std::numeric_limits<uint32_t>::max()));
-
-        double dt = get_elapsed_mcycles();
-        auto dur = (std::chrono::system_clock::now() - wct);
-        auto secs = std::chrono::duration_cast<std::chrono::milliseconds>(dur);
-        std::cout << "@time of GPU run:\t\t\t[" << secs.count() << "] milliseconds" << std::endl;
-        printf("@time of GPU run:\t\t\t[%.3f] million cycles\n", dt);
-        minCyclesISPCGPU = std::min(minCyclesISPCGPU, dt);
-
-        uint64_t contextStart, contextEnd;
-        L0_SAFE_CALL(zeEventGetTimestamp(hTSEvent, ZE_EVENT_TIMESTAMP_CONTEXT_START, &contextStart));
-        L0_SAFE_CALL(zeEventGetTimestamp(hTSEvent, ZE_EVENT_TIMESTAMP_CONTEXT_END, &contextEnd));
-        printf("@time of GPU kernel run:\t\t[%.1f] milliseconds\n", 1e-6*timestampFreq*(contextEnd - contextStart));
-        minKernelClocksGPU = std::min(minKernelClocksGPU, (contextEnd - contextStart));
-    }
-
+    run_kernel(ISPCRT_DEVICE_TYPE_GPU);
     savePPM("ao-ispc-gpu.ppm", width, height, fimg);
-    printf("[aobench ISPC GPU]:\t\t[%.3f] million cycles (%d x %d image)\n", minCyclesISPCGPU, width, height);
 
-    //
     // Run the serial code nIterations times, and report the minimum
     // time for any of them.
-    //
     double minCyclesSerial = 1e30;
-    uint64_t minSerialTimeCPU = std::numeric_limits<uint64_t>::max();
     for (unsigned int i = 0; i < niterations; i++) {
         memset((void *)fimg, 0, sizeof(float) * width * height * 3);
         reset_and_start_timer();
         auto wct = std::chrono::system_clock::now();
         ao_serial(width, height, NSUBSAMPLES, fimg);
-        auto dur = (std::chrono::system_clock::now() - wct);
-        auto secs = std::chrono::duration_cast<std::chrono::milliseconds>(dur);
-        std::cout << "@time of CPU run:\t\t\t[" << secs.count() << "] milliseconds" << std::endl;
         double dt = get_elapsed_mcycles();
+        auto dur = (std::chrono::system_clock::now() - wct);
+        auto secs = std::chrono::duration_cast<std::chrono::nanoseconds>(dur);
+        printf("@time of CPU run:\t\t\t[%ld] nanoseconds\n", secs.count());
         printf("@time of CPU run:\t\t\t[%.3f] million cycles\n", dt);
         minCyclesSerial = std::min(minCyclesSerial, dt);
-        uint64_t nanos = std::chrono::duration_cast<std::chrono::nanoseconds>(dur).count();
-        minSerialTimeCPU = std::min(minSerialTimeCPU, nanos);
     }
     savePPM("ao-cpp-serial.ppm", width, height, fimg);
-
     printf("[aobench serial]:\t\t[%.3f] million cycles (%d x %d image)\n", minCyclesSerial, width, height);
-    printf("\t\t\t\t(%.2fx speedup from ISPC)\n", 1.*minSerialTimeCPU / (minKernelClocksGPU*timestampFreq));
-    free(fimg);
-    L0_SAFE_CALL(zeDriverFreeMem(hDriver, dFimg));
     printf("aobench PASSED!\n");
+    free(img);
+    free(fimg);
     return 0;
 }
 
@@ -253,8 +216,8 @@ int main(int argc, char *argv[]) {
 
     int success = 0;
 
-    std::cout << "Running test with " << niterations << " iterations on " << width << " * " << height << " size."
-              << std::endl;
+    std::cout << "Running test with " << niterations << " iterations of ISPC on GEN and CPU on " << width << " * "
+              << height << " size." << std::endl;
     success = run();
 
     return success;
