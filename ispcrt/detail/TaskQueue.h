9751e0f18feece30ce7a61b4b65a04aabb7427aa
Jan Zielinski <jan.zielinski@intel.com>
2020-05-29 10:23:12 -0700

    [ispcrt] Implement timing of kernel execution

--- ispcrt/detail/TaskQueue.h
+++ ispcrt/detail/TaskQueue.h
@@ -5,6 +5,7 @@
 
 #include "Kernel.h"
 #include "MemoryView.h"
+#include "Future.h"
 
 namespace ispcrt {
 
@@ -17,7 +18,7 @@ struct TaskQueue : public RefCounted {
     virtual void copyToHost(MemoryView &mv) = 0;
     virtual void copyToDevice(MemoryView &mv) = 0;
 
-    virtual void launch(Kernel &k, MemoryView *params, size_t dim0, size_t dim1, size_t dim2) = 0;
+    virtual Future* launch(Kernel &k, MemoryView *params, size_t dim0, size_t dim1, size_t dim2) = 0;
 
     virtual void sync() = 0;
 };
