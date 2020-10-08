4ba720b1b779a19f034c7a7838c788c94ceebf70
Krzysztof Raszkowski <krzysztof.raszkowski@intel.com>
2020-10-08 12:10:30 +0200

    Fix appveyor ci builds

--- scripts/install_emscripten.bat
+++ scripts/install_emscripten.bat
@@ -2,6 +2,7 @@ set WD=%cd%
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
 git pull
+git checkout 2.0.4
 call emsdk.bat install sdk-1.39.11-64bit
 call emsdk.bat activate sdk-1.39.11-64bit
 cd %WD%
