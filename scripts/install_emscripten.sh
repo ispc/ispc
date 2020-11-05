849edcd49db2a25b9c8f0b3416538aa1c18fe994
Dmitry Babokin <dmitry.y.babokin@intel.com>
2020-11-05 14:09:28 -0800

    Used fixed emcc version on Linux

--- scripts/install_emscripten.sh
+++ scripts/install_emscripten.sh
@@ -2,6 +2,7 @@ WD=`pwd`
 git clone https://github.com/emscripten-core/emsdk.git && \
 cd emsdk
 git pull && \
+git checkout 2.0.4 && \
 ./emsdk install sdk-1.39.11-64bit && \
 ./emsdk activate sdk-1.39.11-64bit  && \
 source ./emsdk_env.sh
