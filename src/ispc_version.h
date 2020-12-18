67f5a4e4db40ea4861ac9f62bce7d89f390ee547
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2020-12-18 14:31:37 -0800

    Update version to 1.15.0

--- src/ispc_version.h
+++ src/ispc_version.h
@@ -39,7 +39,7 @@
 
 #define ISPC_VERSION_MAJOR 1
 #define ISPC_VERSION_MINOR 15
-#define ISPC_VERSION "1.15.0dev"
+#define ISPC_VERSION "1.15.0"
 #include <llvm/Config/llvm-config.h>
 
 #define ISPC_LLVM_VERSION (LLVM_VERSION_MAJOR * 10000 + LLVM_VERSION_MINOR * 100)
