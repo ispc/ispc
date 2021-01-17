64a85cb1c8e9cc4621cc6e3561449a0f471f0cb5
Dmitry Babokin <dmitry.y.babokin@intel.com>
2021-01-16 21:37:37 -0800

    Github Actions: install SDE on Windows

--- .github/workflows/scripts/install-test-deps.ps1
+++ .github/workflows/scripts/install-test-deps.ps1
@@ -1,3 +1,6 @@
+choco install --no-progress wget
+
+# Install ISPC package
 $msiexecArgs = @(
     "/i",
     "$pwd\ispc-trunk-windows.msi",
@@ -9,3 +12,9 @@ $msiexecArgs = @(
 Start-Process -FilePath msiexec -ArgumentList $msiexecArgs -NoNewWindow -Wait
 cat install.log
 echo "$pwd\bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+
+# Download and unpack SDE
+wget --retry-connrefused --waitretry=5 --read-timeout=20 --timeout=15 -t 5 https://software.intel.com/content/dam/develop/external/us/en/documents/downloads/${env:SDE_TAR_NAME}-win.tar.bz2
+7z.exe x -tbzip2 ${env:SDE_TAR_NAME}-win.tar.bz2
+7z.exe x -ttar ${env:SDE_TAR_NAME}-win.tar
+echo "$pwd\${env:SDE_TAR_NAME}-win" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
