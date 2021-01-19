c66c2d0a8eb6fd30efc1be4603e9299ed3ea9685
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2021-01-19 11:01:25 -0800

    Check format of src/gen* files

--- check_format.sh
+++ check_format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ##################################################
-#  Copyright (c) 2019-2020, Intel Corporation
+#  Copyright (c) 2019-2021, Intel Corporation
 #  All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
@@ -50,7 +50,7 @@ fi
 
 # Check all source files.
 # For benchmarks folder do not check 03_complex, as these tests come from real projects with their formatting.
-FILES=`ls src/*.cpp src/*.h *.cpp builtins/builtins-c-* benchmarks/{01,02}*/*{cpp,ispc}`
+FILES=`ls src/*.cpp src/*.h src/gen/*.cpp src/gen/*.h *.cpp builtins/builtins-c-* benchmarks/{01,02}*/*{cpp,ispc}`
 for FILE in $FILES; do
     $CLANG_FORMAT $FILE | cmp  $FILE >/dev/null
     if [ $? -ne 0 ]; then
