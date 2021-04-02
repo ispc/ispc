6e10fa77245a7a0cbf8443a311c6ea788f2cf41b
Dmitry Babokin <dmitry.y.babokin@intel.com>
2021-04-01 22:06:41 -0700

    Report error codes in docs building script

--- docs/build.sh
+++ docs/build.sh
@@ -2,14 +2,19 @@
 
 rst2html=rst2html
 
+exit_status=0
 for i in ispc ispc_for_gen perfguide faq; do
     $rst2html --template=template.txt --link-stylesheet \
-        --stylesheet-path=css/style.css $i.rst > $i.html
+        --stylesheet-path=css/style.css --exit-status=2 $i.rst > $i.html
+    (( exit_status = exit_status || $? ))
 done
 
 $rst2html --template=template-perf.txt --link-stylesheet \
-        --stylesheet-path=css/style.css perf.rst > perf.html
+        --stylesheet-path=css/style.css --exit-status=2 perf.rst > perf.html
+(( exit_status = exit_status || $? ))
 
 #rst2latex --section-numbering --documentclass=article --documentoptions=DIV=9,10pt,letterpaper ispc.rst > ispc.tex
 #pdflatex ispc.tex
 #/bin/rm -f ispc.aux ispc.log ispc.out ispc.tex
+
+[ $exit_status -eq 0 ]
