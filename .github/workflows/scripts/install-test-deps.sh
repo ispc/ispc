8150e51a00fd815fb4be5fb449d429635b31a74d
Dmitry Babokin <dmitry.y.babokin@intel.com>
2021-04-01 23:49:40 -0700

    Ignore error during find execution.

--- .github/workflows/scripts/install-test-deps.sh
+++ .github/workflows/scripts/install-test-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 echo "APT::Acquire::Retries \"3\";" | sudo tee -a /etc/apt/apt.conf.d/80-retries
 sudo apt-get update && sudo apt-get install libc6-dev-i386 g++-multilib lib32stdc++6
-find /usr -name cdefs.h
+find /usr -name cdefs.h || echo "Find errors were ignored"
 wget --retry-connrefused --waitretry=5 --read-timeout=20 --timeout=15 -t 5 https://software.intel.com/content/dam/develop/external/us/en/documents/downloads/"$SDE_TAR_NAME"-lin.tar.bz2
 tar xf "$SDE_TAR_NAME"-lin.tar.bz2
 tar xf ispc-trunk-linux.tar.gz
