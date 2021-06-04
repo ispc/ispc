ccc8073379054b399956b26193bcc7ad64fe5608
Dmitry Babokin <dmitry.y.babokin@intel.com>
2021-06-03 23:15:01 -0700

    Remove -Werror from default flags, keep it in CI

--- .github/workflows/scripts/build-ispc.sh
+++ .github/workflows/scripts/build-ispc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 echo PATH=$PATH
-cmake -B build -DISPC_PREPARE_PACKAGE=ON -DISPC_INCLUDE_BENCHMARKS=ON
+cmake -B build -DISPC_PREPARE_PACKAGE=ON -DISPC_INCLUDE_BENCHMARKS=ON -DCMAKE_CXX_FLAGS=-Werror
 cmake --build build --target package -j4
