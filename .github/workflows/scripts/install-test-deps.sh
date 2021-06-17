0239fa2f0a097a3e073df53c2f84a11ce03629ba
Dmitry Babokin <dmitry.y.babokin@intel.com>
2021-06-17 14:25:09 -0700

    Add retry for apt-get failures

--- .github/workflows/scripts/install-test-deps.sh
+++ .github/workflows/scripts/install-test-deps.sh
@@ -1,6 +1,27 @@
 #!/bin/bash -e
 echo "APT::Acquire::Retries \"3\";" | sudo tee -a /etc/apt/apt.conf.d/80-retries
-sudo apt-get update && sudo apt-get install libc6-dev-i386 g++-multilib lib32stdc++6
+
+# if apt-get fails, retry several time.
+for i in {1..5}
+do
+  sudo apt-get update | tee log${i}.txt
+  sudo apt-get install libc6-dev-i386 g++-multilib lib32stdc++6 | tee -a log${i}.txt
+  if [[ ! `grep "Err:" log${i}.txt` ]]; then
+    echo "APT packages installation was successful"
+    break
+  else
+    if [[ ${i} -eq 5 ]]; then
+      echo "APT had unrecoverable errors, exiting"
+      exit 1
+    else
+      sleep_time=$((${i} * 10))
+      echo "APT packages installation failed, sleeping ${sleep_time} seconds"
+      sleep ${sleep_time}
+      sudo rm -rf /var/lib/apt/lists/*
+    fi
+  fi
+done
+
 find /usr -name cdefs.h || echo "Find errors were ignored"
 wget --retry-connrefused --waitretry=5 --read-timeout=20 --timeout=15 -t 5 https://software.intel.com/content/dam/develop/external/us/en/documents/downloads/"$SDE_TAR_NAME"-lin.tar.bz2
 tar xf "$SDE_TAR_NAME"-lin.tar.bz2
