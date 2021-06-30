cedc5f0c72639c46e4545ed25274fe5b701d90c4
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2021-06-30 12:09:35 -0700

    Allow to pass custom SPV to dpcpp example

--- examples/xpu/cmake/TestDrivers.cmake
+++ examples/xpu/cmake/TestDrivers.cmake
@@ -37,7 +37,7 @@ set(TEST_RUNNER "${CMAKE_CURRENT_BINARY_DIR}/runner.py")
 
 function(test_add)
     set(options TEST_IS_CM TEST_IS_ISPC TEST_IS_CM_RUNTIME TEST_IS_ISPCRT_RUNTIME TEST_IS_DPCPP)
-    set(oneValueArgs NAME RES_IMAGE REF_IMAGE IMAGE_CMP_TH)
+    set(oneValueArgs NAME RES_IMAGE REF_IMAGE IMAGE_CMP_TH DPCPP_SPV)
     cmake_parse_arguments("PARSED_ARGS" "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
 
     # If test is written with TEST_IS_CM_RUNTIME it is supported on Windows only
@@ -71,6 +71,9 @@ function(test_add)
     if (PARSED_ARGS_IMAGE_CMP_TH)
         set (IMAGE_CMP_TH_OPT "-image_cmp_th" ${PARSED_ARGS_IMAGE_CMP_TH})
     endif()
+    if (PARSED_ARGS_DPCPP_SPV)
+        set (DPCPP_SPV "-dpcpp_spv" ${PARSED_ARGS_DPCPP_SPV})
+    endif()
 
 
     if (WIN32)
@@ -80,7 +83,7 @@ function(test_add)
     endif()
     if (SUPPORTED EQUAL 1)
         add_test(NAME ${result_test_name}
-            COMMAND ${PYTHON_EXECUTABLE} ${TEST_RUNNER} ${REF_IMAGE_OPT} ${RES_IMAGE_OPT} ${IMAGE_CMP_TH_OPT}
+            COMMAND ${PYTHON_EXECUTABLE} ${TEST_RUNNER} ${REF_IMAGE_OPT} ${RES_IMAGE_OPT} ${IMAGE_CMP_TH_OPT} ${DPCPP_SPV}
             ${TEST_REL_PATH} ${PARSED_ARGS_UNPARSED_ARGUMENTS}
         )
     endif()
