e7c606ab327d496145daa3afadbf1c9afc4606cb
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2021-08-20 16:10:51 -0700

    Mark ESIMD functions as SYCL_ESIMD_FUNCTION

--- examples/xpu/simple-esimd/simple-esimd.cpp
+++ examples/xpu/simple-esimd/simple-esimd.cpp
@@ -8,7 +8,7 @@ using namespace sycl::ext::intel::experimental::esimd;
 
 constexpr unsigned VL = 8;
 
-extern "C" SYCL_EXTERNAL void vmult(simd<float, VL> *a, simd<float, VL> *res, int factor, int n) {
+extern "C" SYCL_EXTERNAL SYCL_ESIMD_FUNCTION void vmult(simd<float, VL> *a, simd<float, VL> *res, int factor, int n) {
     for (int i = 0; i < n / VL; ++i) {
         auto b = a[i];
         res[i] = b * factor;
