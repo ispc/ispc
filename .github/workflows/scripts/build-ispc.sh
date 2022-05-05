2877c98f07e8d52488a31b602af18faf8ab7098f
Dmitry Babokin <dmitry.y.babokin@intel.com>
2022-05-05 15:30:27 -0700

    Enforce ispc package name in CI to make it work for release as well

--- .github/workflows/scripts/build-ispc.sh
+++ .github/workflows/scripts/build-ispc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 echo PATH=$PATH
-cmake -B build -DISPC_PREPARE_PACKAGE=ON -DISPC_INCLUDE_BENCHMARKS=ON -DCMAKE_CXX_FLAGS=-Werror
+cmake -B build -DISPC_PREPARE_PACKAGE=ON -DISPC_INCLUDE_BENCHMARKS=ON -DCMAKE_CXX_FLAGS=-Werror -DISPC_PACKAGE_NAME=ispc-trunk-linux
 cmake --build build --target package -j4
