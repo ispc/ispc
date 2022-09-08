39ff13c60eec52cbf987316999c6856d9c65be6f
Dmitry Babokin <dmitry.y.babokin@intel.com>
2022-09-08 16:22:24 -0700

    Temporarily disable deprecation warning for the build to build with deprecated opaque pointers API

--- .github/workflows/scripts/build-ispc.sh
+++ .github/workflows/scripts/build-ispc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 echo PATH=$PATH
-cmake -B build -DISPC_PREPARE_PACKAGE=ON -DISPC_INCLUDE_BENCHMARKS=ON -DCMAKE_CXX_FLAGS=-Werror -DISPC_PACKAGE_NAME=ispc-trunk-linux $@
+# FIXME: -Wno-deprecated-declarations is here only until support for opaque pointers is not implemented in ISPC
+cmake -B build -DISPC_PREPARE_PACKAGE=ON -DISPC_INCLUDE_BENCHMARKS=ON -DCMAKE_CXX_FLAGS="-Werror -Wno-deprecated-declarations" -DISPC_PACKAGE_NAME=ispc-trunk-linux $@
 cmake --build build --target package -j4
