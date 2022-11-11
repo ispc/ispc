4c2b5b740646531a07d7921cb1ef98f0d33a1a84
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2022-11-10 16:02:49 -0800

    Added src/opt directory to check_format.sh

--- check_format.sh
+++ check_format.sh
@@ -50,7 +50,7 @@ fi
 
 # Check all source files.
 # For benchmarks folder do not check 03_complex, as these tests come from real projects with their formatting.
-FILES=`ls src/*.cpp src/*.h *.cpp builtins/builtins-c-* benchmarks/{01,02}*/*{cpp,ispc} common/*.h stdlib.ispc`
+FILES=`ls src/*.cpp src/*.h src/opt/*.cpp src/opt/*.h *.cpp builtins/builtins-c-* benchmarks/{01,02}*/*{cpp,ispc} common/*.h stdlib.ispc`
 for FILE in $FILES; do
     $CLANG_FORMAT $FILE | cmp  $FILE >/dev/null
     if [ $? -ne 0 ]; then
