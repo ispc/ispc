cda3870d2403d94fa378f8384de0e9c0e53ac478
Alexey Nurmukhametov <alex@nurmukhametov.ru>
2022-12-20 22:17:11 +0000

    Add check_copyright.sh script

---
+++ check_copyright.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright 2022 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+
+REF="$1"
+
+export YEAR=$(date +%Y)
+
+# List files with Copyright whether they have up-to-date date.
+git diff --name-only "$REF" | xargs -I {} bash -c 'test -f {} && (grep -q "Copyright.*$YEAR" {} || (grep -q Copyright {} && echo {}))' | ( ! grep ^ )
