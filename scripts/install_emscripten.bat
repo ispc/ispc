ddbe81a0a4205084e5a72e5c7b5497671a98574a
Lectem <lectem@gmail.com>
2023-02-27 22:56:58 +0100

    Downgrade emscripten version to 3.1.17 as we are still using llvm15
    emscripten 3.1.31 upgraded to llvm17 and 3.1.18 to llvm16

--- scripts/install_emscripten.bat
+++ scripts/install_emscripten.bat
@@ -2,7 +2,7 @@ set WD=%cd%
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
 git pull
-git checkout 3.1.31
-call emsdk.bat install 3.1.31
-call emsdk.bat activate 3.1.31
+git checkout 3.1.17
+call emsdk.bat install 3.1.17
+call emsdk.bat activate 3.1.17
 cd %WD%
--- scripts/install_emscripten.sh
+++ scripts/install_emscripten.sh
@@ -2,8 +2,8 @@ WD=`pwd`
 git clone https://github.com/emscripten-core/emsdk.git && \
 cd emsdk
 git pull && \
-git checkout 3.1.31 && \
-./emsdk install 3.1.31 && \
-./emsdk activate 3.1.31 && \
+git checkout 3.1.17 && \
+./emsdk install 3.1.17 && \
+./emsdk activate 3.1.17 && \
 source ./emsdk_env.sh
 cd $WD
