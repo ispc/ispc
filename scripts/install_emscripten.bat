05b422b422859ce6900e18762c899111630ae6f6
Lectem <lectem@gmail.com>
2023-02-27 01:03:42 +0100

    Bump emsdk version to 3.1.31

--- scripts/install_emscripten.bat
+++ scripts/install_emscripten.bat
@@ -2,7 +2,7 @@ set WD=%cd%
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
 git pull
-git checkout 2.0.4
-call emsdk.bat install sdk-1.39.11-64bit
-call emsdk.bat activate sdk-1.39.11-64bit
+git checkout 3.1.31
+call emsdk.bat install 3.1.31
+call emsdk.bat activate 3.1.31
 cd %WD%
--- scripts/install_emscripten.sh
+++ scripts/install_emscripten.sh
@@ -2,8 +2,8 @@ WD=`pwd`
 git clone https://github.com/emscripten-core/emsdk.git && \
 cd emsdk
 git pull && \
-git checkout 2.0.4 && \
-./emsdk install sdk-1.39.11-64bit && \
-./emsdk activate sdk-1.39.11-64bit  && \
+git checkout 3.1.31 && \
+./emsdk install 3.1.31 && \
+./emsdk activate 3.1.31 && \
 source ./emsdk_env.sh
 cd $WD
