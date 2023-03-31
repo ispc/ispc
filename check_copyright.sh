4ba07eea072834b20e12ba40b4b465e135cabc04
Dmitry Babokin <dmitry.y.babokin@intel.com>
2023-03-31 15:19:48 -0700

    Update copyright check to work with longer paths (increase buffer 256->1024)

--- check_copyright.sh
+++ check_copyright.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2022, Intel Corporation
+# Copyright (c) 2022-2023, Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 
 REF="$1"
@@ -7,4 +7,4 @@ REF="$1"
 export YEAR=$(date +%Y)
 
 # List files with Copyright whether they have up-to-date date.
-git diff --name-only "$REF" | xargs -I {} bash -c 'test -f {} && (grep -q "Copyright.*$YEAR" {} || (grep -q Copyright {} && echo {}))' | ( ! grep ^ )
+git diff --name-only "$REF" | xargs -I {} -S 1024 bash -c 'test -f {} && (grep -q "Copyright.*$YEAR" {} || (grep -q Copyright {} && echo {}))' | ( ! grep ^ )
