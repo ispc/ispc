5be26e8cfa808add317eddd367f44de2612e97f0
Dmitry Babokin <dmitry.y.babokin@intel.com>
2023-04-19 17:28:06 -0700

    Use zip archive package in Windows CI

--- .github/workflows/scripts/install-test-deps.ps1
+++ .github/workflows/scripts/install-test-deps.ps1
@@ -2,17 +2,9 @@
 Choco-Install -PackageName wget
 
 # Install ISPC package
-$msiexecArgs = @(
-    "/i",
-    "$pwd\ispc-trunk-windows.msi",
-    "/L*V",
-    "$pwd\install.log",
-    "/qn",
-    "INSTALL_ROOT=$pwd"
-)
-Start-Process -FilePath msiexec -ArgumentList $msiexecArgs -NoNewWindow -Wait
-cat install.log
-echo "$pwd\bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+Expand-Archive $pwd\ispc-trunk-windows.zip -DestinationPath $pwd
+ls
+echo "$pwd\ispc-trunk-windows\bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
 
 # Download and unpack SDE
 wget -U "${env:USER_AGENT}" --retry-connrefused --waitretry=5 --read-timeout=20 --timeout=15 -t 5 https://downloadmirror.intel.com/${env:SDE_MIRROR_ID}/${env:SDE_TAR_NAME}-win.tar.xz
