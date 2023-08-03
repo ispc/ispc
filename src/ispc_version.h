58f4b007dcd6f935906d023593cb505176a43263
Dmitry Babokin <dmitry.y.babokin@intel.com>
2023-08-02 21:28:08 -0700

    Add LLVM 18.0 definition

--- src/ispc_version.h
+++ src/ispc_version.h
@@ -20,9 +20,10 @@
 #define ISPC_LLVM_15_0 150000
 #define ISPC_LLVM_16_0 160000
 #define ISPC_LLVM_17_0 170000
+#define ISPC_LLVM_18_0 180000
 
 #define OLDEST_SUPPORTED_LLVM ISPC_LLVM_13_0
-#define LATEST_SUPPORTED_LLVM ISPC_LLVM_17_0
+#define LATEST_SUPPORTED_LLVM ISPC_LLVM_18_0
 
 #ifdef __ispc__xstr
 #undef __ispc__xstr
@@ -34,7 +35,7 @@
     __ispc__xstr(LLVM_VERSION_MAJOR) "." __ispc__xstr(LLVM_VERSION_MINOR) "." __ispc__xstr(LLVM_VERSION_PATCH)
 
 #if ISPC_LLVM_VERSION < OLDEST_SUPPORTED_LLVM || ISPC_LLVM_VERSION > LATEST_SUPPORTED_LLVM
-#error "Only LLVM 13.0 - 16.0 and 17.0 development branch are supported"
+#error "Only LLVM 13.0 - 17.0 and 18.0 development branch are supported"
 #endif
 
 #define ISPC_VERSION_STRING                                                                                            \
