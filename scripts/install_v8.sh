9145e63de47a661195998ede301fbc7b77b8f633
i-saint <saint.skr@gmail.com>
2023-08-15 15:33:00 +0900

    run_tests.py for wasm64

--- scripts/install_v8.sh
+++ scripts/install_v8.sh
@@ -2,7 +2,7 @@ WD=`pwd`
 sudo apt-get install nodejs && \
 npm install jsvu -g && \
 export PATH="${HOME}/.jsvu:${PATH}" && \
-jsvu --os=linux64 v8-debug@8.4.200 && \
+jsvu --os=linux64 v8-debug@10.2.154 && \
 cp scripts/v8-redirect.sh ${HOME}/.jsvu/v8 && \
 chmod +x ${HOME}/.jsvu/v8
 cd $WD
