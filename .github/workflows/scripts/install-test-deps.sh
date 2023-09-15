447d48d26c32243678418bf87c4c201e3e037335
Aleksei Nurmukhametov <aleksei.nurmukhametov@intel.com>
2023-09-15 04:51:03 -0700

    CI: add job to build and tests XE_ENABLED build

--- .github/workflows/scripts/install-test-deps.sh
+++ .github/workflows/scripts/install-test-deps.sh
@@ -30,6 +30,19 @@ wget -U "$USER_AGENT" --retry-connrefused --waitretry=5 --read-timeout=20 --time
 tar xf "$SDE_TAR_NAME"-lin.tar.xz
 tar xf ispc-trunk-linux.tar.gz
 
+if [ -v INSTALL_COMPUTE_RUNTIME ]; then
+    echo "install Compute Runtime"
+    wget -qO - https://repositories.intel.com/graphics/intel-graphics.key | sudo gpg --dearmor --output /usr/share/keyrings/intel-graphics.gpg
+    echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/intel-graphics.gpg] https://repositories.intel.com/graphics/ubuntu jammy unified' > /tmp/intel.gpu.focal.list
+    sudo mv /tmp/intel.gpu.focal.list /etc/apt/sources.list.d/
+    sudo apt-get -y update
+    sudo apt-get --no-install-recommends install -y intel-opencl-icd \
+        intel-media-va-driver-non-free libmfx1 libmfxgen1 libvpl2 \
+        libegl-mesa0 libegl1-mesa libegl1-mesa-dev libgbm1 libgl1-mesa-dev libgl1-mesa-dri \
+        libglapi-mesa libgles2-mesa-dev libglx-mesa0 libigdgmm12 libxatracker2 mesa-va-drivers \
+        mesa-vdpau-drivers mesa-vulkan-drivers va-driver-all
+fi
+
 #GA requires to set env putting value to $GITHUB_ENV & $GITHUB_PATH
 echo "SDE_HOME=$GITHUB_WORKSPACE/$SDE_TAR_NAME-lin" >> $GITHUB_ENV
 echo "$GITHUB_WORKSPACE/ispc-trunk-linux/bin" >> $GITHUB_PATH
