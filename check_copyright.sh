687d60f3205446d1e10167045f6329547b5ab935
Aleksei Nurmukhametov <aleksei.nurmukhametov@intel.com>
2024-01-25 08:44:29 -0800

    Repair check_copyright script
    It was broken (on Linux and effectively in CI) since 2b37147ba417113
    Only xargs on macOS has -S replsize option.
    So, avoid passing this flag when xargs doesn't support it.

--- check_copyright.sh
+++ check_copyright.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-# Copyright (c) 2022-2023, Intel Corporation
+# Copyright (c) 2022-2024, Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 
 REF="$1"
 
 export YEAR=$(date +%Y)
 
+REPLARG="-S 1024"
+if !(xargs $REPLARG 2>/dev/null); then
+    # xargs doesn't support -S argument, so do not use it
+    REPLARG=""
+fi
+
 # List files with Copyright whether they have up-to-date date.
-git diff --name-only "$REF" | xargs -I {} -S 1024 bash -c 'test -f {} && (grep -q "Copyright.*$YEAR" {} || (grep -q Copyright {} && echo {}))' | ( ! grep ^ )
+git diff --name-only "$REF" | xargs -I {} $REPLARG bash -c 'test -f {} && (grep -q "Copyright.*$YEAR" {} || (grep -q Copyright {} && echo {}))' | ( ! grep ^ )
