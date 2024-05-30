fc416f23599ca252cdcabaf5bbe86ef46ec98adf
Aleksei Nurmukhametov <aleksei.nurmukhametov@intel.com>
2024-05-30 16:56:06 +0100

    Remove LLVM_14 ifdefs

--- src/ispc_version.h
+++ src/ispc_version.h
@@ -15,14 +15,13 @@
 
 #define ISPC_LLVM_VERSION (LLVM_VERSION_MAJOR * 10000 + LLVM_VERSION_MINOR * 100)
 
-#define ISPC_LLVM_14_0 140000
 #define ISPC_LLVM_15_0 150000
 #define ISPC_LLVM_16_0 160000
 #define ISPC_LLVM_17_0 170000
 #define ISPC_LLVM_18_1 180100
 #define ISPC_LLVM_19_0 190000
 
-#define OLDEST_SUPPORTED_LLVM ISPC_LLVM_14_0
+#define OLDEST_SUPPORTED_LLVM ISPC_LLVM_15_0
 #define LATEST_SUPPORTED_LLVM ISPC_LLVM_19_0
 
 #ifdef __ispc__xstr
@@ -35,7 +34,7 @@
     __ispc__xstr(LLVM_VERSION_MAJOR) "." __ispc__xstr(LLVM_VERSION_MINOR) "." __ispc__xstr(LLVM_VERSION_PATCH)
 
 #if ISPC_LLVM_VERSION < OLDEST_SUPPORTED_LLVM || ISPC_LLVM_VERSION > LATEST_SUPPORTED_LLVM
-#error "Only LLVM 14.0 - 18.1 and 19.0 development branch are supported"
+#error "Only LLVM 15.0 - 18.1 and 19.0 development branch are supported"
 #endif
 
 #define ISPC_VERSION_STRING                                                                                            \
--- src/llvmutil.h
+++ src/llvmutil.h
@@ -22,16 +22,6 @@
 #include <llvm/Support/ModRef.h>
 #endif
 
-// In the transition to Opaque Pointers getElementType() was deprecated, getPointerElementType() will live a little
-// longer. But we need another solution eventually. Issue #2245 was filed to track this.
-#if ISPC_LLVM_VERSION >= ISPC_LLVM_14_0
-#define PTR_ELT_TYPE getPointerElementType
-#else
-#define PTR_ELT_TYPE getElementType
-#endif
-
-#define PTYPE(p) (llvm::cast<llvm::PointerType>((p)->getType()->getScalarType())->PTR_ELT_TYPE())
-
 namespace llvm {
 class PHINode;
 class InsertElementInst;
