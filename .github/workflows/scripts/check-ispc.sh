8254ac75734f6861aac327c95dce949fe1aea687
Aleksei Nurmukhametov <aleksei.nurmukhametov@intel.com>
2024-05-31 15:20:08 +0100

    CI: add job linux-aarch64-build-ispc

--- .github/workflows/scripts/check-ispc.sh
+++ .github/workflows/scripts/check-ispc.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -e
+#
+# Copyright 2024, Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+
 cd build
 bin/check_isa
 bin/ispc --support-matrix
 if [[ $OSTYPE == 'darwin'* ]]; then
   cmake --build . --target check-all
 else
-  cmake --build . --target check-all ispc_benchmarks test
+  ARCH=$(uname -m)
+  if [ "$ARCH" == "aarch64" ]; then
+    cmake --build . --target check-all
+  else
+    cmake --build . --target check-all ispc_benchmarks test
+  fi
 fi
