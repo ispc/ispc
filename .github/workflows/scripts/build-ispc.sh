7b25b3bec3792927f0eef37d56e2be3624bc564b
Aleksei Nurmukhametov <aleksei.nurmukhametov@intel.com>
2024-05-31 15:20:08 +0100

    CI: add job linux-aarch64-build-ispc

--- .github/workflows/scripts/build-ispc.sh
+++ .github/workflows/scripts/build-ispc.sh
@@ -1,12 +1,33 @@
 #!/bin/bash -e
+#
+# Copyright 2024, Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+
 echo PATH=$PATH
 
 if [[ $OSTYPE == 'darwin'* ]]; then
   # macOS is treated differently because we don't build benchmarks and package name is different.
   # Benchmarks are not built, because Github Action macOS shared runners run on too old hardware -
   # IvyBridge, i.e. AVX1, while our benchmark setup assumes at least AVX2.
-  cmake -B build -DISPC_PREPARE_PACKAGE=ON -DCMAKE_CXX_FLAGS="-Werror" -DISPC_MACOS_UNIVERSAL_BINARIES=ON -DISPC_PACKAGE_NAME=ispc-trunk-macos -DISPC_OPAQUE_PTR_MODE=${ISPC_OPAQUE_PTR_MODE} $@
+  cmake -B build \
+    -DISPC_MACOS_UNIVERSAL_BINARIES=ON \
+    -DISPC_PREPARE_PACKAGE=ON \
+    -DCMAKE_CXX_FLAGS="-Werror" \
+    -DISPC_PACKAGE_NAME=ispc-trunk-macos \
+    -DISPC_OPAQUE_PTR_MODE=${ISPC_OPAQUE_PTR_MODE} \
+    $@
 else
-  cmake -B build -DISPC_PREPARE_PACKAGE=ON -DISPC_INCLUDE_BENCHMARKS=ON -DCMAKE_CXX_FLAGS="-Werror" -DISPC_PACKAGE_NAME=ispc-trunk-linux -DISPC_OPAQUE_PTR_MODE=${ISPC_OPAQUE_PTR_MODE} $@
+  ARCH=$(uname -m)
+  BENCHMARKS=ON
+  if [ "$ARCH" == "aarch64" ]; then
+    BENCHMARKS=OFF
+  fi
+  cmake -B build \
+    -DISPC_INCLUDE_BENCHMARKS=${BENCHMARKS} \
+    -DISPC_PREPARE_PACKAGE=ON \
+    -DCMAKE_CXX_FLAGS="-Werror" \
+    -DISPC_PACKAGE_NAME=ispc-trunk-linux \
+    -DISPC_OPAQUE_PTR_MODE=${ISPC_OPAQUE_PTR_MODE} \
+    $@
 fi
 cmake --build build --target package -j4
