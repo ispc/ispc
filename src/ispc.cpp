bec9369a99b832e1dd1c323ebca9a9bc468c4ae7
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2025-01-15 13:10:49 -0800

    Use armv8a instead of armv7for 'arm' arch

--- src/ispc.cpp
+++ src/ispc.cpp
@@ -2424,7 +2424,7 @@ std::string Target::GetTripleString() const {
         } else if (m_arch == Arch::x86_64) {
             triple.setArchName("x86_64");
         } else if (m_arch == Arch::arm) {
-            triple.setArchName("armv7");
+            triple.setArchName("armv8a");
         } else if (m_arch == Arch::aarch64) {
             triple.setArchName("aarch64");
         } else if (m_arch == Arch::xe64) {
@@ -2458,7 +2458,7 @@ std::string Target::GetTripleString() const {
         } else if (m_arch == Arch::x86_64) {
             triple.setArchName("amd64");
         } else if (m_arch == Arch::arm) {
-            triple.setArchName("armv7");
+            triple.setArchName("armv8a");
         } else if (m_arch == Arch::aarch64) {
             triple.setArchName("aarch64");
         } else {
@@ -2487,7 +2487,7 @@ std::string Target::GetTripleString() const {
         } else if (m_arch == Arch::x86_64) {
             triple.setArchName("x86_64");
         } else if (m_arch == Arch::arm) {
-            triple.setArchName("armv7");
+            triple.setArchName("armv8a");
         } else if (m_arch == Arch::aarch64) {
             triple.setArchName("aarch64");
         } else {
