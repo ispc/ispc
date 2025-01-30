bbb9c9b1e9489c124c108796f5f76953cc802d7a
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2025-01-30 14:39:45 -0800

    Add definitions for LLVM 20

--- src/ispc_version.h
+++ src/ispc_version.h
@@ -19,9 +19,10 @@
 #define ISPC_LLVM_18_1 180100
 #define ISPC_LLVM_19_0 190000
 #define ISPC_LLVM_20_0 200000
+#define ISPC_LLVM_21_0 210000
 
 #define OLDEST_SUPPORTED_LLVM ISPC_LLVM_17_0
-#define LATEST_SUPPORTED_LLVM ISPC_LLVM_20_0
+#define LATEST_SUPPORTED_LLVM ISPC_LLVM_21_0
 
 #ifdef __ispc__xstr
 #undef __ispc__xstr
@@ -33,7 +34,7 @@
     __ispc__xstr(LLVM_VERSION_MAJOR) "." __ispc__xstr(LLVM_VERSION_MINOR) "." __ispc__xstr(LLVM_VERSION_PATCH)
 
 #if ISPC_LLVM_VERSION < OLDEST_SUPPORTED_LLVM || ISPC_LLVM_VERSION > LATEST_SUPPORTED_LLVM
-#error "Only LLVM 17.0 - 19.1 and 20.0 development branch are supported"
+#error "Only LLVM 17.0 - 20.1 and 21.0 development branch are supported"
 #endif
 
 #define ISPC_VERSION_STRING                                                                                            \
