99f0b1c837888d2e31fb96d86ea018e21704ba52
Aleksei Nurmukhametov <aleksei.nurmukhametov@intel.com>
2025-04-28 16:35:12 +0100

    CI: add linux-test-ispc-wasm job

--- scripts/install_emscripten.sh
+++ scripts/install_emscripten.sh
@@ -2,7 +2,7 @@ WD=`pwd`
 git clone https://github.com/emscripten-core/emsdk.git && \
 cd emsdk
 git pull && \
-./emsdk install 3.1.17 && \
-./emsdk activate 3.1.17  && \
+./emsdk install 4.0.7 && \
+./emsdk activate 4.0.7  && \
 source ./emsdk_env.sh
 cd $WD
