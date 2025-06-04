035cf7b3fb6a73e9579d23233795786d11003395
Aleksei Nurmukhametov <aleksei.nurmukhametov@intel.com>
2025-06-04 14:35:56 +0100

    avx[1|2]-i64x4: clean-up builtins

--- cmake/GenerateBuiltins.cmake
+++ cmake/GenerateBuiltins.cmake
@@ -46,7 +46,6 @@ list(APPEND M4_IMPLICIT_DEPENDENCIES
     builtins/svml.m4
     builtins/target-avx-utils.ll
     builtins/target-avx-common-8.ll
-    builtins/target-avx1-i64x4base.ll
     builtins/target-avx512-common-4.ll
     builtins/target-avx512-common-8.ll
     builtins/target-avx512-common-16.ll
