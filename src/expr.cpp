8d31bfd0f66ea9e5497072b821ed755be4869a97
Jérôme Richard <jeromerichard111@msn.com>
2025-06-13 22:09:02 +0200

    Fix #2291 about extracting the address of const variables set to compile-time constants

--- src/expr.cpp
+++ src/expr.cpp
@@ -6144,7 +6144,7 @@ ConstExpr::ConstExpr(const Type *t, bool *b, SourcePos p) : Expr(p, ConstExprID)
     }
 }
 
-ConstExpr::ConstExpr(const ConstExpr *old, SourcePos p) : Expr(p, ConstExprID) {
+ConstExpr::ConstExpr(const ConstExpr *old, SourcePos p, unsigned scid) : Expr(p, scid) {
     type = old->type;
 
     AtomicType::BasicType basicType = getBasicType();
@@ -6187,6 +6187,10 @@ ConstExpr::ConstExpr(const ConstExpr *old, SourcePos p) : Expr(p, ConstExprID) {
         FATAL("unimplemented const type");
     }
 }
+
+ConstExpr::ConstExpr(const ConstExpr *old, SourcePos p) : ConstExpr(old, p, ConstExprID) {}
+
+ConstExpr::ConstExpr(const Symbol *s, SourcePos p) : ConstExpr(s->constValue, p, ConstSymbolExprID) {}
 
 AtomicType::BasicType ConstExpr::getBasicType() const {
     const AtomicType *at = CastType<AtomicType>(type);
@@ -6718,6 +6722,48 @@ std::string ConstExpr::GetString() const { return GetValuesAsStr(", "); }
 
 void ConstExpr::Print(Indent &indent) const {
     indent.Print("ConstExpr", pos);
+
+    printf("[%s] (", GetTypeUnsafe()->GetString().c_str());
+    printf("%s", GetValuesAsStr((char *)", ").c_str());
+    printf(")\n");
+
+    indent.Done();
+}
+
+///////////////////////////////////////////////////////////////////////////
+// ConstSymbolExpr
+
+ConstSymbolExpr::ConstSymbolExpr(const ConstSymbolExpr *old, SourcePos p)
+    : ConstExpr(old->symbol, p), symbol(old->symbol) {}
+
+ConstSymbolExpr::ConstSymbolExpr(Symbol *s, SourcePos p) : ConstExpr(s, p), symbol(s) {}
+
+llvm::Value *ConstSymbolExpr::GetLValue(FunctionEmitContext *ctx) const {
+    if (symbol->storageInfo == nullptr) {
+        return nullptr;
+    }
+    ctx->SetDebugPos(pos);
+    return symbol->storageInfo->getPointer();
+}
+
+const Type *ConstSymbolExpr::GetLValueType() const {
+    if (CastType<ReferenceType>(symbol->type) != nullptr) {
+        return PointerType::GetUniform(symbol->type->GetReferenceTarget());
+    } else {
+        return PointerType::GetUniform(symbol->type);
+    }
+}
+
+ConstSymbolExpr *ConstSymbolExpr::Instantiate(TemplateInstantiation &templInst) const {
+    Symbol *resolvedSymbol = templInst.InstantiateSymbol(symbol);
+    AssertPos(pos, resolvedSymbol != nullptr);
+    return new ConstSymbolExpr(resolvedSymbol, pos);
+}
+
+Symbol *ConstSymbolExpr::GetBaseSymbol() const { return symbol; }
+
+void ConstSymbolExpr::Print(Indent &indent) const {
+    indent.Print("ConstSymbolExpr", pos);
 
     printf("[%s] (", GetTypeUnsafe()->GetString().c_str());
     printf("%s", GetValuesAsStr((char *)", ").c_str());
@@ -8867,7 +8913,7 @@ Expr *SymbolExpr::Optimize() {
         return nullptr;
     } else if (symbol->constValue != nullptr) {
         AssertPos(pos, GetType()->IsConstType());
-        return new ConstExpr(symbol->constValue, pos);
+        return new ConstSymbolExpr(symbol, pos);
     } else {
         return this;
     }
