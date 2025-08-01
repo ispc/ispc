4dbcaa0916ae346b1e59b45723860d7042968567
Aleksei Nurmukhametov <aleksei.nurmukhametov@intel.com>
2025-08-01 15:27:02 +0100

    Remove conditional code for LLVM 17

--- src/ispc_version.h
+++ src/ispc_version.h
@@ -15,14 +15,13 @@
 
 #define ISPC_LLVM_VERSION (LLVM_VERSION_MAJOR * 10000 + LLVM_VERSION_MINOR * 100)
 
-#define ISPC_LLVM_17_0 170000
 #define ISPC_LLVM_18_1 180100
 #define ISPC_LLVM_19_0 190000
 #define ISPC_LLVM_20_0 200000
 #define ISPC_LLVM_21_0 210000
 #define ISPC_LLVM_22_0 220000
 
-#define OLDEST_SUPPORTED_LLVM ISPC_LLVM_17_0
+#define OLDEST_SUPPORTED_LLVM ISPC_LLVM_18_1
 #define LATEST_SUPPORTED_LLVM ISPC_LLVM_22_0
 
 #ifdef __ispc__xstr
@@ -35,7 +34,7 @@
     __ispc__xstr(LLVM_VERSION_MAJOR) "." __ispc__xstr(LLVM_VERSION_MINOR) "." __ispc__xstr(LLVM_VERSION_PATCH)
 
 #if ISPC_LLVM_VERSION < OLDEST_SUPPORTED_LLVM || ISPC_LLVM_VERSION > LATEST_SUPPORTED_LLVM
-#error "Only LLVM 17.0 - 21.1 and 22.0 development branch are supported"
+#error "Only LLVM 18.1 - 21.1 and 22.0 development branch are supported"
 #endif
 
 #define ISPC_VERSION_STRING                                                                                            \
