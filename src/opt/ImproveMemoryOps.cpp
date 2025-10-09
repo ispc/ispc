65270a8f3e62214dee9306de31abcc85dbf326cf
Karthik Senthil <karthik.senthil@intel.com>
2025-10-09 16:22:57 -0700

    [ImproveMemOps] Do not truncate offset computation for unsigned loop counters. (#3581)
    * [ImproveMemOps] Do not truncate offset computation for 64-bit targets.
    
    Currently ImproveMemOps pass tries to truncate all offset computations to 32-bits
    even for 64-bit targets. This is not conducive for downstream LLVM passes like
    InstCombine when incoming ISPC source has unsigned loop induction variable. It ends
    up generating a sext instead of zext while computing addresses using the unsigned
    induction variable. This PR updates ImproveMemOps to not truncate offsets when
    target is 64-bit, to keep inline with ISPC frontend. Additionally it also updates
    ISPC frontend to not emit nsw flags on binops using unsigned loop induction variable.
    
    Github issue: #3238
    
    * Address review comments.
    
    - Make the fix in ImproveMemoryOps less aggressive to prevent loss of
      optimizations from 32-bit addressing. We now use sext vs. zext to
      identify when to not truncate offset computation.
    - Rename and fix LIT test for --target=host.

--- src/opt/ImproveMemoryOps.cpp
+++ src/opt/ImproveMemoryOps.cpp
@@ -624,10 +624,10 @@ static bool lOffsets32BitSafe(llvm::Value **variableOffsetPtr, llvm::Value **con
     if (variableOffset->getType() != LLVMTypes::Int32VectorType) {
         if (auto *castInst = llvm::dyn_cast<llvm::CastInst>(variableOffset)) {
             auto opcode = castInst->getOpcode();
-            if ((opcode == llvm::Instruction::ZExt || opcode == llvm::Instruction::SExt) &&
-                castInst->getOperand(0)->getType() == LLVMTypes::Int32VectorType) {
-
-                // zext or sext of a 32-bit vector -> the 32-bit vector is good
+            if (opcode == llvm::Instruction::SExt && castInst->getOperand(0)->getType() == LLVMTypes::Int32VectorType) {
+                // sext of a 32-bit vector -> the 32-bit vector is good.
+                // Don't allow zext as they are used by ispc frontend to denote offsets based on unsigned loop counter
+                // variable.
                 variableOffset = castInst->getOperand(0);
             } else {
                 return false;
@@ -701,10 +701,14 @@ static bool lOffsets32BitSafe(llvm::Value **offsetPtr, llvm::Instruction *insert
     }
 
     llvm::SExtInst *sext = llvm::dyn_cast<llvm::SExtInst>(offset);
+    llvm::ZExtInst *zext = llvm::dyn_cast<llvm::ZExtInst>(offset);
     if (sext != nullptr && sext->getOperand(0)->getType() == LLVMTypes::Int32VectorType) {
         // sext of a 32-bit vector -> the 32-bit vector is good
         *offsetPtr = sext->getOperand(0);
         return true;
+    } else if (zext != nullptr && zext->getOperand(0)->getType() == LLVMTypes::Int32VectorType) {
+        // Don't allow zext as they are used by ispc frontend to denote offsets based on unsigned loop counter variable.
+        return false;
     } else if (lIs32BitSafeHelper(offset)) {
         // The only constant vector we should have here is a vector of
         // all zeros (i.e. a ConstantAggregateZero, but just in case,
