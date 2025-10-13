a8acb3fb81a96e4683cff657adc5b19e7e18d70b
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2025-10-13 14:58:10 -0700

    CI: use pwsh and enforce pwsh scripts error ocde propagation

--- .github/workflows/scripts/build-ispc.ps1
+++ .github/workflows/scripts/build-ispc.ps1
@@ -1,2 +1,4 @@
 cmake -B build -DISPC_PREPARE_PACKAGE=ON -DISPC_CROSS=ON -DISPC_INCLUDE_BENCHMARKS=ON -DCMAKE_BUILD_TYPE="${env:BUILD_TYPE}" -DISPC_GNUWIN32_PATH="${env:CROSS_TOOLS_GNUWIN32}" -DISPC_PACKAGE_NAME=ispc-trunk-windows @args
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 cmake --build build --target package -j4 --config ${env:BUILD_TYPE}
+exit $LASTEXITCODE
--- .github/workflows/scripts/check-ispc.ps1
+++ .github/workflows/scripts/check-ispc.ps1
@@ -4,3 +4,4 @@ ispc.exe --binary-type
 ispc.exe --support-matrix
 ispc.exe --check-bitcode-libs
 cmake --build build --target check-all --config ${env:BUILD_TYPE}
+exit $LASTEXITCODE
