ca4bae33b48e45645781dcf6de8c12687b3d84e8
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2025-10-16 14:52:08 -0700

    Add avx512icl-x16-nozmm target

--- src/target_enums.cpp
+++ src/target_enums.cpp
@@ -164,8 +164,11 @@ ISPCTarget operator++(ISPCTarget &target, int dummy) {
     static_assert(static_cast<underlying>(ISPCTarget::avx512icl_x16) ==
                       static_cast<underlying>(ISPCTarget::avx512icl_x8) + 1,
                   "Enum ISPCTarget is not sequential");
-    static_assert(static_cast<underlying>(ISPCTarget::avx512icl_x32) ==
+    static_assert(static_cast<underlying>(ISPCTarget::avx512icl_x16_nozmm) ==
                       static_cast<underlying>(ISPCTarget::avx512icl_x16) + 1,
+                  "Enum ISPCTarget is not sequential");
+    static_assert(static_cast<underlying>(ISPCTarget::avx512icl_x32) ==
+                      static_cast<underlying>(ISPCTarget::avx512icl_x16_nozmm) + 1,
                   "Enum ISPCTarget is not sequential");
     static_assert(static_cast<underlying>(ISPCTarget::avx512icl_x64) ==
                       static_cast<underlying>(ISPCTarget::avx512icl_x32) + 1,
@@ -419,6 +422,8 @@ ISPCTarget ParseISPCTarget(std::string target) {
         return ISPCTarget::avx512icl_x8;
     } else if (target == "avx512icl-x16") {
         return ISPCTarget::avx512icl_x16;
+    } else if (target == "avx512icl-x16-nozmm") {
+        return ISPCTarget::avx512icl_x16_nozmm;
     } else if (target == "avx512icl-x32") {
         return ISPCTarget::avx512icl_x32;
     } else if (target == "avx512icl-x64") {
@@ -616,6 +621,7 @@ std::string ISPCTargetToString(ISPCTarget target) {
     case ISPCTarget::avx512icl_x8:
         return "avx512icl-x8";
     case ISPCTarget::avx512icl_x16:
+    case ISPCTarget::avx512icl_x16_nozmm:
         return "avx512icl-x16";
     case ISPCTarget::avx512icl_x32:
         return "avx512icl-x32";
@@ -755,6 +761,7 @@ bool ISPCTargetIsX86(ISPCTarget target) {
     case ISPCTarget::avx512icl_x4:
     case ISPCTarget::avx512icl_x8:
     case ISPCTarget::avx512icl_x16:
+    case ISPCTarget::avx512icl_x16_nozmm:
     case ISPCTarget::avx512icl_x32:
     case ISPCTarget::avx512icl_x64:
     case ISPCTarget::avx512spr_x4:
