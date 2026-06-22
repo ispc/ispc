2422afeb6ebfd20921b1b304fbded7571461986f
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2026-06-22 15:13:45 -0700

    Switch to LLVM 23 by default

--- .github/workflows/scripts/init-env.sh
+++ .github/workflows/scripts/init-env.sh
@@ -5,7 +5,7 @@
 SDE_REPO=${SDE_REPO:-"https://github.com/ispc/ispc.dependencies"}
 SDE_TAR_NAME=${SDE_TAR_NAME:-"sde-external-10.8.0-2026-03-15"}
 LLVM_REPO=${LLVM_REPO:-"https://github.com/ispc/ispc.dependencies"}
-LLVM_VERSION=${LLVM_VERSION:-"22.1"}
+LLVM_VERSION=${LLVM_VERSION:-"23.1"}
 echo "LLVM_VERSION=${LLVM_VERSION}" >> "${GITHUB_ENV}"
 
 OS=$(uname -s)
@@ -13,13 +13,13 @@ ARCH=$(uname -m)
 case "$OS" in
     Linux*)
       if [ "$ARCH" == "aarch64" ]; then
-        LLVM_TAR=${LLVM_TAR:-"llvm-22.1.6-ubuntu22.04aarch64-Release+Asserts-x86.arm.wasm.tar.xz"}
+        LLVM_TAR=${LLVM_TAR:-"llvm-23.1.0-ubuntu22.04aarch64-Release+Asserts-x86.arm.wasm.tar.xz"}
       else
-        LLVM_TAR=${LLVM_TAR:-"llvm-22.1.6-ubuntu22.04-Release+Asserts-x86.arm.wasm.tar.xz"}
+        LLVM_TAR=${LLVM_TAR:-"llvm-23.1.0-ubuntu22.04-Release+Asserts-x86.arm.wasm.tar.xz"}
       fi
       ;;
     Darwin*)
-      LLVM_TAR=${LLVM_TAR:-"llvm-22.1.6-macos-Release+Asserts-universal-x86.arm.wasm.tar.xz"}
+      LLVM_TAR=${LLVM_TAR:-"llvm-23.1.0-macos-Release+Asserts-universal-x86.arm.wasm.tar.xz"}
       ;;
     *)
       echo "Unsupported OS: $OS"
